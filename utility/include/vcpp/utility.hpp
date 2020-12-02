7e7ea14a323c64153a479b7c605c9801fda936ce
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-02 10:55:49 +0900

    add utility

---
+++ utility/include/vcpp/utility.hpp
@@ -0,0 +1,11 @@
+//
+// Created by YongGyu Lee on 2020/12/01.
+//
+
+#ifndef VCPP_UTILITY_HPP
+#define VCPP_UTILITY_HPP
+
+#include "vcpp/utility/at.hpp"
+#include "vcpp/utility/sequence.hpp"
+
+#endif //VCPP_UTILITY_HPP
