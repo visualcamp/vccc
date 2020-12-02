492e7e58e4f71933fd0a53d0685ff74ddfa2b06e
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-02 10:54:53 +0900

    Update default_type_traits.hpp

--- type_traits/include/vcpp/type_traits/default_type_traits.hpp
+++ type_traits/include/vcpp/type_traits/default_type_traits.hpp
@@ -100,6 +100,124 @@ struct is_std_array<std::array<T, n>> : std::true_type {};
 template<typename ...Types>
 using decay_if_float_t = typename std::conditional_t<are_<std::is_same<Types, float>...>::value, float, double>;
 
+
+/**
+ *  returns bigger type
+ */
+
+template<typename ...>
+struct bigger_type;
+
+template<typename T>
+struct bigger_type<T>;
+
+template<typename T1, typename T2>
+struct bigger_type<T1, T2>;
+
+template<typename T1, typename T2, typename ...Ts>
+struct bigger_type<T1, T2, Ts...>;
+
+template<typename T>
+struct bigger_type<T> {
+  static_assert(!std::is_integral<T>::value, "Use signed_bigger_type or unsigned_bigger_type for integer types");
+  using type = T;
+};
+
+template<typename T1, typename T2,typename ...Ts>
+struct bigger_type<T1, T2, Ts...> {
+  using bt = typename bigger_type<T1, T2>::type;
+  using type = typename bigger_type<bt, Ts...>::type;
+};
+
+template<typename T1, typename T2>
+struct bigger_type<T1, T2> {
+  using type = std::conditional_t<sizeof(T1) >= sizeof(T2), typename bigger_type<T1>::type, typename bigger_type<T2>::type>;
+};
+
+template<typename ...Ts>
+using bigger_type_t = typename bigger_type<Ts...>::type;
+
+
+/**
+ *  returns signed bigger type of Ts
+ *  if unsigned types are provided, smaller unsigned types will be returned
+ */
+template<typename ...>
+struct signed_bigger_type;
+
+template<typename T>
+struct signed_bigger_type<T>;
+
+template<typename T1, typename T2>
+struct signed_bigger_type<T1, T2>;
+
+template<typename T1, typename T2, typename ...Ts>
+struct signed_bigger_type<T1, T2, Ts...>;
+
+template<typename T>
+struct signed_bigger_type<T> {
+  static_assert(std::is_integral<T>::value, "Use bigger_type for non-integral types");
+  using type = T;
+};
+
+template<typename T1, typename T2, typename ...Ts>
+struct signed_bigger_type<T1, T2, Ts...> {
+  using bt = typename signed_bigger_type<T1, T2>::type;
+  using type = typename signed_bigger_type<bt, Ts...>::type;
+};
+
+template<typename T1, typename T2>
+struct signed_bigger_type<T1, T2> {
+  using type =
+  std::conditional_t<sizeof(T1) >= sizeof(T2) and std::is_signed<T1>::value,
+                     typename signed_bigger_type<T1>::type,
+                     typename signed_bigger_type<T2>::type>;
+};
+
+template<typename ...Ts>
+using signed_bigger_type_t = typename signed_bigger_type<Ts...>::type;
+
+
+/**
+ *  returns unsigned bigger type of Ts
+ *  if signed types are provided, smaller signed types will be returned
+ */
+
+template<typename ...>
+struct unsigned_bigger_type;
+
+template<typename T>
+struct unsigned_bigger_type<T>;
+
+template<typename T1, typename T2>
+struct unsigned_bigger_type<T1, T2>;
+
+template<typename T1, typename T2, typename ...Ts>
+struct unsigned_bigger_type<T1, T2, Ts...>;
+
+template<typename T>
+struct unsigned_bigger_type<T> {
+  static_assert(std::is_integral<T>::value, "Use bigger_type for non-integral types");
+  using type = T;
+};
+
+template<typename T1, typename T2, typename ...Ts>
+struct unsigned_bigger_type<T1, T2, Ts...> {
+  using bt = typename unsigned_bigger_type<T1, T2>::type;
+  using type = typename unsigned_bigger_type<bt, Ts...>::type;
+};
+
+template<typename T1, typename T2>
+struct unsigned_bigger_type<T1, T2> {
+  using type =
+  std::conditional_t<sizeof(T1) >= sizeof(T2) and std::is_unsigned<T1>::value,
+                     typename unsigned_bigger_type<T1>::type,
+                     typename unsigned_bigger_type<T2>::type>;
+};
+
+template<typename ...Ts>
+using unsigned_bigger_type_t = typename unsigned_bigger_type<Ts...>::type;
+
 }
 
 #endif //VCPP_TYPE_TRAITS_DEFAULT_TYPE_TRAITS_HPP
