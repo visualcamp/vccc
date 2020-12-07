6da277a0dd85424883bc685061c732028f64ad3b
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-07 17:02:43 +0900

    move `at` on opencv types to type_support

--- type_support/include/vccc/type_support.hpp
+++ type_support/include/vccc/type_support.hpp
@@ -5,6 +5,7 @@
 #ifndef VCCC_TYPE_SUPPORT_PP
 #define VCCC_TYPE_SUPPORT_PP
 
+#include "vccc/type_support/at.hpp"
 #include "vccc/type_support/opencv_size.hpp"
 #include "vccc/type_support/convert_to.hpp"
 #include "vccc/type_support/vtype_convert.hpp"
