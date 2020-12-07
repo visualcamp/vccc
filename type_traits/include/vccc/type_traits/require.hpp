c907689a2d791ac2cb2b447667c3f45e7978d3d6
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-07 16:52:14 +0900

    change header file name

--- type_traits/include/vccc/type_traits/require.hpp
+++ type_traits/include/vccc/type_traits/require.hpp
@@ -2,8 +2,8 @@
 // Created by YongGyu Lee on 2020/12/01.
 //
 
-#ifndef VCCC_TYPE_TRAITS_MACRO_HPP
-#define VCCC_TYPE_TRAITS_MACRO_HPP
+#ifndef VCCC_TYPE_TRAITS_REQUIRE_HPP
+#define VCCC_TYPE_TRAITS_REQUIRE_HPP
 
 
 #define VCCC_REQUIRE(Cond) std::enable_if_t<Cond, int> = 0
@@ -13,4 +13,4 @@
 #define VCCC_REQUIRE_IMPL(Cond) std::enable_if_t<Cond, int>
 
 
-#endif //VCCC_TYPE_TRAITS_MACRO_HPP
+#endif //VCCC_TYPE_TRAITS_REQUIRE_HPP
