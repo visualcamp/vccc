7bdffbec06322120fc4633a65936bed49d34aa2f
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-07 16:56:59 +0900

    move bind_at to separate file

--- utility/include/vccc/utility/at.hpp
+++ utility/include/vccc/utility/at.hpp
@@ -443,43 +443,6 @@ template<std::size_t i, std::size_t j, typename C, typename T> auto constexpr at
 
 #endif
 
-/**
- *  call at<...>(t) to args...
- */
-
-namespace detail{
-
-template<typename ...Ts>
-class bind_obj {
- public:
-  template<typename ...Args>
-  constexpr bind_obj(Args&&... args) : tup(std::forward<Args>(args)...) {}
-
-  template<typename T>
-  constexpr bind_obj& operator = (T&& type) {
-    bind_impl(std::forward<T>(type), std::index_sequence_for<Ts...>{});
-    return *this;
-  }
-
- private:
-  template<typename T, std::size_t ...I>
-  constexpr void bind_impl(T&& type, std::index_sequence<I...>) {
-    [[maybe_unused]] volatile int dummy[sizeof...(I)] = {
-        (at<I>(tup) = at<I>(type), 0)...
-    };
-  }
-
-
-  std::tuple<Ts...> tup;
-};
-
-}
-
-template<typename ...Args>
-constexpr detail::bind_obj<Args...> bind_at(Args&&... args) {
-  return detail::bind_obj<Args...>(std::forward<Args>(args)...);
-}
-
 }
 
 #endif //VCCC_UTILITY_AT_HPP
