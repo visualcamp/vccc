52372576ea8be9f684f6112bdc17f93e85b86218
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-07 17:25:12 +0900

    update `at`

--- utility/include/vccc/utility/at.hpp
+++ utility/include/vccc/utility/at.hpp
@@ -10,7 +10,7 @@
 #include <iterator>
 
 #include "vccc/type_traits.hpp"
-#include "vccc/utility/assert.h"
+#include "vccc/utility/assert.hpp"
 
 namespace vc{
 
@@ -20,58 +20,112 @@ namespace vc{
 template<std::size_t i, typename T,
     VCCC_REQUIRE(i < std::tuple_size<T>::value)>
 constexpr
-decltype(auto) at (T& tuple) {
+decltype(auto)
+at (T& tuple) {
   return std::get<i>(tuple);
 }
 
 template<std::size_t i, typename T,
     VCCC_REQUIRE(i < std::tuple_size<T>::value)>
 constexpr
-decltype(auto) at (const T& tuple) {
+decltype(auto)
+at (const T& tuple) {
   return std::get<i>(tuple);
+}
+
+template<std::size_t i, typename T,
+    VCCC_REQUIRE(i < std::tuple_size<T>::value)>
+constexpr
+decltype(auto)
+at (T&& tuple) {
+  return std::move(std::get<i>(tuple));
+}
+
+template<std::size_t i, typename T,
+    VCCC_REQUIRE(i < std::tuple_size<T>::value)>
+constexpr
+decltype(auto)
+at (const T&& tuple) {
+  return std::move(std::get<i>(tuple));
 }
 
 
 /** container */
 
 template<std::size_t i, typename Container,
-  VCCC_REQUIRE(is_container_v<Container>)>
-decltype(auto) at(Container& container) {
+         VCCC_REQUIRE(is_container_v<Container>)>
+decltype(auto)
+at(Container& container) {
   BOUNDS_ASSERT(i, container.size());
   return *std::next(std::begin(container), i);
 }
 
 template<std::size_t i, typename Container,
-  VCCC_REQUIRE(is_container_v<Container>)>
-decltype(auto) at(const Container& container) {
+         VCCC_REQUIRE(is_container_v<Container>)>
+decltype(auto)
+at(const Container& container) {
   BOUNDS_ASSERT(i, container.size());
   return *std::next(std::begin(container), i);
+}
+
+template<std::size_t i, typename Container,
+         VCCC_REQUIRE(is_container_v<Container>)>
+decltype(auto)
+at(Container&& container) {
+  BOUNDS_ASSERT(i, container.size());
+  return std::move(*std::next(std::begin(container), i));
+}
+
+template<std::size_t i, typename Container,
+         VCCC_REQUIRE(is_container_v<Container>)>
+decltype(auto)
+at(const Container&& container) {
+  BOUNDS_ASSERT(i, container.size());
+  return std::move(*std::next(std::begin(container), i));
 }
 
 
 /** std::array */
 
-template<std::size_t i, typename T, int n,
-    VCCC_REQUIRE(i < n)>
+template<std::size_t i, typename T, int n>
 constexpr
-decltype(auto) at(std::array<T, n>& arr) {
+decltype(auto)
+at(std::array<T, n>& arr) {
+  static_assert(i < n, "Index out of bounds in at<> (std::array)");
   return arr[i];
 }
 
-template<std::size_t i, typename T, int n,
-    VCCC_REQUIRE(i < n)>
+template<std::size_t i, typename T, int n>
 constexpr
-decltype(auto) at(const std::array<T, n>& arr) {
+decltype(auto)
+at(const std::array<T, n>& arr) {
+  static_assert(i < n, "Index out of bounds in at<> (const std::array)");
   return arr[i];
+}
+
+template<std::size_t i, typename T, int n>
+constexpr
+T&&
+at(std::array<T, n>&& arr) {
+  static_assert(i < n, "Index out of bounds in at<> (std::array&&)");
+  return std::move(arr[i]);
+}
+
+template<std::size_t i, typename T, int n>
+constexpr
+const T&&
+at(const std::array<T, n>&& arr) {
+  static_assert(i < n, "Index out of bounds in at<> (const std::array&&)");
+  return std::move(arr[i]);
 }
 
 
 /** variadic */
 
-template<std::size_t i, typename ...Args,
-    VCCC_REQUIRE(i < sizeof...(Args))>
+template<std::size_t i, typename ...Args>
 constexpr
 decltype(auto) variadic_at(Args&&... args){
+  static_assert(i < sizeof...(Args), "Index out of bounds in at<> (Args&&...)");
   return std::get<i>(std::forward_as_tuple(std::forward<Args>(args)...));
 }
 
