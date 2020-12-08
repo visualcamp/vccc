689025968e1a81079efb29322ef81177ae532699
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-08 18:00:42 +0900

    add assert

--- utility/include/vccc/utility.hpp
+++ utility/include/vccc/utility.hpp
@@ -5,7 +5,7 @@
 #ifndef VCCC_UTILITY_HPP
 #define VCCC_UTILITY_HPP
 
-#include "vccc/utility/at.hpp"
+#include "vccc/utility/assert.hpp"
 #include "vccc/utility/sequence.hpp"
 #include "vccc/utility/time.hpp"
 
