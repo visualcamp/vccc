8001220874a2c06fecd0275bfc193efdb0ccc585
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-09 15:56:09 +0900

    Create back_emplace.hpp

---
+++ type_support/include/vccc/type_support/back_emplace.hpp
@@ -0,0 +1,43 @@
+# /*
+#   Created by YongGyu Lee on 2020/12/08.
+# */
+#
+# ifndef VCCC_TYPE_SUPPORT_BACK_EMPLACE_HPP
+# define VCCC_TYPE_SUPPORT_BACK_EMPLACE_HPP
+#
+# include <iterator>
+
+namespace vc{
+
+template <class Container>
+class back_emplace_iterator
+    : public std::iterator<std::output_iterator_tag,
+                           void,
+                           void,
+                           void,
+                           void>
+{
+ protected:
+  Container* container;
+ public:
+  using container_type = Container;
+
+  explicit back_emplace_iterator(Container& x) : container(std::addressof(x)) {}
+  back_emplace_iterator& operator=(const typename Container::value_type& value)
+  {container->emplace_back(value); return *this;}
+  back_emplace_iterator& operator=(typename Container::value_type&& value)
+  {container->emplace_back(std::move(value)); return *this;}
+  back_emplace_iterator& operator*()     {return *this;}
+  back_emplace_iterator& operator++()    {return *this;}
+  back_emplace_iterator  operator++(int) {return *this;}
+};
+
+template<typename Container>
+back_emplace_iterator<Container>
+back_emplacer(Container& container){
+  return back_emplace_iterator<Container>(container);
+}
+
+}
+
+#endif //VCCC_TYPE_SUPPORT_BACK_EMPLACE_HPP
