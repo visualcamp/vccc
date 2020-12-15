36d9669cda769b4038010b706b0910087ba4cec5
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-15 15:56:46 +0900

    remove temporary object

--- type_support/include/vccc/type_support/opencv/cv_vec.hpp
+++ type_support/include/vccc/type_support/opencv/cv_vec.hpp
@@ -52,10 +52,13 @@ at(const cv::Vec<T, n>&& vec) {
 
 
 /** add */
-// TODO: remove temporary instance
+
 template<typename T, int cn, typename N>
 cv::Vec<T, cn> add(const cv::Vec<T, cn>& vec, N n) {
-  return vec + cv::Vec<T, cn>::all(n);
+  auto copy = vec;
+  for(int i=0; i<cn; ++i)
+    copy[i] += n;
+  return copy;
 }
 
 }
