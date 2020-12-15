2c4e1ff9ff90f3dfcbb6ea5910eef786a0568026
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-15 22:52:23 +0900

    Update invoke.hpp

--- high_ver/include/vccc/high_ver/functional/detail/invoke.hpp
+++ high_ver/include/vccc/high_ver/functional/detail/invoke.hpp
@@ -36,62 +36,76 @@ template<class T, class T1>
 using get_invoke_category_t = typename get_invoke_category<T, T1>::type;
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP_MF(invoke_category_base_of,
-                                    Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP_MF(invoke_category_base_of, Type T::* f, T1&& t1, Args&&... args) {
   return (std::forward<T1>(t1).*f)(std::forward<Args>(args)...);
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP_MF(invoke_category_reference_wrapper,
-                                    Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP_MF(invoke_category_reference_wrapper, Type T::* f, T1&& t1, Args&&... args) {
   return (t1.get().*f)(std::forward<Args>(args)...);
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP_MF(invoke_category_normal,
-                                    Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP_MF(invoke_category_normal, Type T::* f, T1&& t1, Args&&... args) {
   return ((*std::forward<T1>(t1)).*f)(std::forward<Args>(args)...);
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP(invoke_category_base_of,
-                                 Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP(invoke_category_base_of, Type T::* f, T1&& t1, Args&&... args) {
   return std::forward<T1>(t1).*f;
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP(invoke_category_reference_wrapper,
-                                 Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP(invoke_category_reference_wrapper, Type T::* f, T1&& t1, Args&&... args) {
   return t1.get().*f;
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKEP(invoke_category_normal,
-                                 Type T::* f, T1&& t1, Args&&... args) {
+constexpr
+decltype(auto)
+INVOKEP(invoke_category_normal, Type T::* f, T1&& t1, Args&&... args) {
   return (*std::forward<T1>(t1)).*f;
 }
 
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKE_TAG1(std::true_type, Type T::* f, T1&& t1, Args&&... args)
+constexpr
+decltype(auto)
+INVOKE_TAG1(std::true_type, Type T::* f, T1&& t1, Args&&... args)
 {
   return INVOKEP_MF(get_invoke_category_t<T, T1>{}, f, std::forward<T1>(t1), std::forward<Args>(args)...);
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKE_TAG1(std::false_type, Type T::* f, T1&& t1, Args&&... args)
+constexpr
+decltype(auto)
+INVOKE_TAG1(std::false_type, Type T::* f, T1&& t1, Args&&... args)
 {
   return INVOKEP(get_invoke_category_t<T, T1>{}, f, std::forward<T1>(t1), std::forward<Args>(args)...);
 }
 
 template <class T, class Type, class T1, class... Args>
-constexpr decltype(auto) INVOKE(Type T::* f, T1&& t1, Args&&... args)
+constexpr
+decltype(auto)
+INVOKE(Type T::* f, T1&& t1, Args&&... args)
 {
   return INVOKE_TAG1(std::is_member_function_pointer<decltype(f)>{}, f, std::forward<T1>(t1), std::forward<Args>(args)...);
 }
 
 template <class F, class... Args>
-constexpr decltype(auto) INVOKE(F&& f, Args&&... args)
+constexpr
+decltype(auto)
+INVOKE(F&& f, Args&&... args)
 {
   return std::forward<F>(f)(std::forward<Args>(args)...);
 }
