8b8fadb1fe3d7439126406aa82b25afd686ef460
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-15 22:38:21 +0900

    Update literal.hpp

--- literal/include/vccc/literal.hpp
+++ literal/include/vccc/literal.hpp
@@ -1,10 +1,10 @@
-//
-// Created by YongGyu Lee on 2020/12/01.
-//
-
-#ifndef VCCC_LITERAL_HPP
-#define VCCC_LITERAL_HPP
-
-#include "literal/formatter.hpp"
-
+# /*
+#  * Created by YongGyu Lee on 2020/12/08.
+#  */
+#
+# ifndef VCCC_LITERAL_HPP
+# define VCCC_LITERAL_HPP
+#
+# include "literal/formatter.hpp"
+#
 # endif //VCCC_LITERAL_HPP
