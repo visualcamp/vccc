480d3b8f6d928aead15321e81df89f8d737f604d
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-15 22:42:17 +0900

    Create high_ver.hpp

---
+++ high_ver/include/vccc/high_ver.hpp
@@ -0,0 +1,11 @@
+# /*
+#  * Created by YongGyu Lee on 2020/12/08.
+#  */
+#
+# ifndef VCCC_HIGH_VER_HPP
+# define VCCC_HIGH_VER_HPP
+#
+# include "vccc/high_ver/functional.hpp"
+# include "vccc/high_ver/tuple.hpp"
+#
+# endif //VCCC_HIGH_VER_HPP
