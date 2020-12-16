1b0cb54314f755757df1c4d77bca121e59b73dd9
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-16 11:40:55 +0900

    add vector concat

--- type_support/include/vccc/type_support/std/vector.hpp
+++ type_support/include/vccc/type_support/std/vector.hpp
@@ -16,6 +16,24 @@ std::vector<T...> reserved_vector(typename std::vector<T...>::size_type size){
   return vec;
 }
 
+template<typename T, typename Allocator>
+std::vector<T, Allocator>& concat(std::vector<T, Allocator>& to, const std::vector<T, Allocator>& from) {
+  static_assert(std::is_copy_constructible<T>::value, "");
+  to.reserve(to.size() + from.size());
+  for(int i=0; i<from.size(); ++i)
+    to.emplace_back(from[i]);
+  return to;
+}
+
+template<typename T, typename Allocator>
+std::vector<T, Allocator>& concat(std::vector<T, Allocator>& to, std::vector<T, Allocator>&& from) {
+  static_assert(std::is_move_constructible<T>::value, "");
+  to.reserve(to.size() + from.size());
+  for(int i=0; i<from.size(); ++i)
+    to.emplace_back(std::move(from[i]));
+  return to;
+}
+
 }
 
 # endif //VCCC_TYPE_SUPPORT_STD_VECTOR_HPP
