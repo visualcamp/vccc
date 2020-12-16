01cee68b054f944e8e9fce3ba2bb069b62e25d04
Tony <35574936+lackhole@users.noreply.github.com>
2020-12-16 11:37:07 +0900

    Update array.hpp

--- type_support/include/vccc/type_support/std/array.hpp
+++ type_support/include/vccc/type_support/std/array.hpp
@@ -13,7 +13,7 @@ template<std::size_t i, typename T, int n>
 constexpr
 decltype(auto)
 at(std::array<T, n>& arr) {
-  static_assert(i < n, "Index out of bounds in at<> (std::array)");
+  static_assert(i < n, "Index out of bounds in vccc::at<> (std::array)");
   return arr[i];
 }
 
@@ -21,7 +21,7 @@ template<std::size_t i, typename T, int n>
 constexpr
 decltype(auto)
 at(const std::array<T, n>& arr) {
-  static_assert(i < n, "Index out of bounds in at<> (const std::array)");
+  static_assert(i < n, "Index out of bounds in vccc::at<> (const std::array)");
   return arr[i];
 }
 
@@ -29,7 +29,7 @@ template<std::size_t i, typename T, int n>
 constexpr
 T&&
 at(std::array<T, n>&& arr) {
-  static_assert(i < n, "Index out of bounds in at<> (std::array&&)");
+  static_assert(i < n, "Index out of bounds in vccc::at<> (std::array&&)");
   return std::move(arr[i]);
 }
 
@@ -37,7 +37,7 @@ template<std::size_t i, typename T, int n>
 constexpr
 const T&&
 at(const std::array<T, n>&& arr) {
-  static_assert(i < n, "Index out of bounds in at<> (const std::array&&)");
+  static_assert(i < n, "Index out of bounds in vccc::at<> (const std::array&&)");
   return std::move(arr[i]);
 }
 
