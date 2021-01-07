ea2ecd080379bc57c80ba752b7afe1b8f8cabb56
Tony <cosgenio@gmail.com>
2021-01-07 16:05:25 +0900

    change all log macro to use fprintf

--- log/include/vccc/log/detail/pc.hpp
+++ log/include/vccc/log/detail/pc.hpp
@@ -13,16 +13,16 @@
 #ifdef VCCC_LOG_DEBUG
 #define EXPAND(x) x
 
-  #define PRINT_WITH_NEWLINE(fmt, ...) EXPAND(std::printf(fmt "\n", __VA_ARGS__))
+  #define PRINT_WITH_NEWLINE(fmt, ...)       EXPAND(std::printf(fmt "\n", __VA_ARGS__))
   #define FPRINT_WITH_NEWLINE(out, fmt, ...) EXPAND(std::fprintf(out, fmt "\n", __VA_ARGS__))
 
-  #define LOGV_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
-  #define LOGD_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
-  #define LOGI_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
-  #define LOGW_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
+  #define LOGV_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+  #define LOGD_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+  #define LOGI_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+  #define LOGW_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
   #define LOGE_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stderr, __VA_ARGS__))
-  #define LOGF_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
-  #define LOGS_IMPL(...) EXPAND(PRINT_WITH_NEWLINE(__VA_ARGS__))
+  #define LOGF_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+  #define LOGS_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
 #else
   #define LOGV_IMPL(...)
   #define LOGD_IMPL(...)
