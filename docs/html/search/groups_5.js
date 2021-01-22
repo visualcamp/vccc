7ca635935f767d0be8da6a4aa33fd1fe87c7b886
Tony <35574936+lackhole@users.noreply.github.com>
2021-01-22 16:27:32 +0900

    update docs

--- docs/html/search/groups_5.js
+++ docs/html/search/groups_5.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['is_5fcontainer_651',['Is_container',['../group__is__container.html',1,'']]],
+  ['is_5fcontainer_651',['is_container',['../group__is__container.html',1,'']]],
   ['is_5feven_652',['is_even',['../group__is__even.html',1,'']]],
-  ['is_5fiterator_653',['Is_iterator',['../group__is__iterator.html',1,'']]],
+  ['is_5fiterator_653',['is_iterator',['../group__is__iterator.html',1,'']]],
   ['is_5fodd_654',['is_odd',['../group__is__odd.html',1,'']]],
-  ['is_5fprintable_655',['Is_printable',['../group__is__printable.html',1,'']]],
-  ['is_5fspecialization_656',['Is_specialization',['../group__is__specialization.html',1,'']]],
-  ['is_5fstd_5farray_657',['Is_std_array',['../group__is__std__array.html',1,'']]],
-  ['is_5fstring_5flike_658',['Is_string_like',['../group__is__string__like.html',1,'']]],
+  ['is_5fprintable_655',['is_printable',['../group__is__printable.html',1,'']]],
+  ['is_5fspecialization_656',['is_specialization',['../group__is__specialization.html',1,'']]],
+  ['is_5fstd_5farray_657',['is_std_array',['../group__is__std__array.html',1,'']]],
+  ['is_5fstring_5flike_658',['is_string_like',['../group__is__string__like.html',1,'']]],
   ['integer_20sequence_659',['Integer Sequence',['../group__utility__sequence.html',1,'']]]
 ];
