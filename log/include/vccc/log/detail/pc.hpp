5817f56b2490cecaea9fc34d2fe09a8eca2c78bb
Tony <35574936+lackhole@users.noreply.github.com>
2021-01-29 15:21:44 +0900

    fix log on iOS

--- log/include/vccc/log/detail/pc.hpp
+++ log/include/vccc/log/detail/pc.hpp
@@ -1,36 +1,32 @@
-//
-// Created by YongGyu Lee on 2020/12/01.
-//
-
-#ifndef VCCC_LOG_PC_HPP
-#define VCCC_LOG_PC_HPP
-
-#include <cstdio>
-
-// TODO split debug macro
-#define VCCC_LOG_DEBUG
-
-#ifdef VCCC_LOG_DEBUG
-#define EXPAND(x) x
-
-  #define PRINT_WITH_NEWLINE(fmt, ...)       EXPAND(std::printf(fmt "\n", __VA_ARGS__))
-  #define FPRINT_WITH_NEWLINE(out, fmt, ...) EXPAND(std::fprintf(out, fmt "\n", __VA_ARGS__))
-
-  #define LOGV_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-  #define LOGD_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-  #define LOGI_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-  #define LOGW_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-  #define LOGE_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stderr, __VA_ARGS__))
-  #define LOGF_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-  #define LOGS_IMPL(...) EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
-#else
-  #define LOGV_IMPL(...)
-  #define LOGD_IMPL(...)
-  #define LOGI_IMPL(...)
-  #define LOGW_IMPL(...)
-  #define LOGE_IMPL(...)
-  #define LOGF_IMPL(...)
-  #define LOGS_IMPL(...)
+# /*
+#   Created by YongGyu Lee on 2020/12/01.
+# */
+# ifndef VCCC_LOG_PC_HPP
+# define VCCC_LOG_PC_HPP
+#
+# include <cstdio>
+#
+# ifdef NDEBUG
+#     define LOGV_IMPL(...)
+#     define LOGD_IMPL(...)
+#     define LOGI_IMPL(...)
+#     define LOGW_IMPL(...)
+#     define LOGE_IMPL(...)
+#     define LOGF_IMPL(...)
+#     define LOGS_IMPL(...)
+# else
+#     define VCCC_DETAIL_EXPAND(x) x // defense for MSVC
+#
+#     define PRINT_WITH_NEWLINE(fmt, ...)       VCCC_DETAIL_EXPAND(std::printf(fmt "\n", __VA_ARGS__))
+#     define FPRINT_WITH_NEWLINE(out, fmt, ...) VCCC_DETAIL_EXPAND(std::fprintf(out, fmt "\n", __VA_ARGS__))
+#
+#     define LOGV_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+#     define LOGD_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+#     define LOGI_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+#     define LOGW_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+#     define LOGE_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stderr, __VA_ARGS__))
+#     define LOGF_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
+#     define LOGS_IMPL(...) VCCC_DETAIL_EXPAND(FPRINT_WITH_NEWLINE(stdout, __VA_ARGS__))
 # endif
-
+#
 # endif //VCCC_LOG_PC_HPP
--- log/include/vccc/log/detail/platform_log.hpp
+++ log/include/vccc/log/detail/platform_log.hpp
@@ -4,18 +4,15 @@
 #
 # ifndef VCCC_LOG_PLATFORM_LOG
 # define VCCC_LOG_PLATFORM_LOG
-
-/**
- *      If you add another header for that platform, it must
- *      define VCCC_LOG_DEBUG when really writes out
- */
-
-# ifdef __ANDROID__
-  # include "vccc/log/detail/android.hpp"
-# elif __APPLE__ && TARGET_OS_IPHONE
-  # include "vccc/log/detail/ios.hpp"
+#
+# include "boost/predef.h"
+#
+# if BOOST_PLAT_ANDROID != BOOST_VERSION_NUMBER_NOT_AVAILABLE
+#     include "vccc/log/detail/android.hpp"
+# elif BOOST_OS_IOS != BOOST_VERSION_NUMBER_NOT_AVAILABLE
+#     include "vccc/log/detail/ios.hpp"
 # else
-  # include "vccc/log/detail/pc.hpp"
+#     include "vccc/log/detail/pc.hpp"
 # endif
-
+#
 # endif //VCCC_LOG_PLATFORM_LOG
