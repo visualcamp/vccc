8f21dc69dd66370c80361e6141395335790b69cc
Tony <35574936+lackhole@users.noreply.github.com>
2021-02-02 12:34:43 +0900

    [hotfix] fix ios debug

--- log/include/vccc/log/detail/ios.hpp
+++ log/include/vccc/log/detail/ios.hpp
@@ -7,20 +7,20 @@
 # include "TargetConditionals.h"
 # include <syslog.h>
 #
-# ifdef NDEBUG
-#     define LOGV_IMPL(...)
-#     define LOGD_IMPL(...)
-#     define LOGI_IMPL(...)
-#     define LOGW_IMPL(...)
-#     define LOGF_IMPL(...)
-#     define LOGS_IMPL(...)
-# else
+# ifdef DEBUG
 #     define LOGV_IMPL(...) syslog(LOG_NOTICE, __VA_ARGS__);
 #     define LOGD_IMPL(...) syslog(LOG_DEBUG, __VA_ARGS__);
 #     define LOGI_IMPL(...) syslog(LOG_INFO, __VA_ARGS__);
 #     define LOGW_IMPL(...) syslog(LOG_WARNING, __VA_ARGS__);
 #     define LOGF_IMPL(...) syslog(LOG_ERR, __VA_ARGS__);
 #     define LOGS_IMPL(...) syslog(LOG_ERR, __VA_ARGS__);
+# else
+#     define LOGV_IMPL(...)
+#     define LOGD_IMPL(...)
+#     define LOGI_IMPL(...)
+#     define LOGW_IMPL(...)
+#     define LOGF_IMPL(...)
+#     define LOGS_IMPL(...)
 # endif
 #
 # define LOGE_IMPL(...) syslog(LOG_ERR, __VA_ARGS__);
