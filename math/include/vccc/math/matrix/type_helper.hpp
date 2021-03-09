3202df46a2f055a47bb147c75e5661b27974cbcb
Tony <35574936+lackhole@users.noreply.github.com>
2021-03-09 11:07:41 +0900

    fix hold_type_selector

--- math/include/vccc/math/matrix/type_helper.hpp
+++ math/include/vccc/math/matrix/type_helper.hpp
@@ -16,11 +16,11 @@ namespace internal{ namespace math{
 template<typename T>
 struct hold_type_selector {
   using type =
-      std::conditional_t<traits<T>::option & Flag::kAliasUnsafe,
+      std::conditional_t<bool(traits<T>::option & Flag::kReferenceUnsafe),
                          const std::remove_reference_t<T>,
                          const T&>;
   using non_const_type =
-      std::conditional_t<traits<T>::option & Flag::kAliasUnsafe,
+      std::conditional_t<bool(traits<T>::option & Flag::kReferenceUnsafe),
                          std::remove_reference_t<T>,
                          T&>;
 };
