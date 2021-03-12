d012f2eff8fa04d71435de293831e4a260270b7c
Tony <35574936+lackhole@users.noreply.github.com>
2021-03-12 14:55:31 +0900

    add test

---
+++ log/test/log_test.cpp
@@ -0,0 +1,32 @@
+//
+// Created by YongGyu Lee on 2021/02/03.
+//
+
+#include "test_core.hpp"
+#include <cmath>
+#include <vccc/log.hpp>
+#include <array>
+
+
+int main() {
+  INIT_TEST("vccc::log")
+
+  LOGD(1);
+
+  std::cout << std::boolalpha;
+
+
+  char str[10] = {'h', 'e', 'l', 'l', 'o'};
+
+  LOGD("%s", str);
+
+  std::cout << vccc::detail::are_types_c_printable<const char*, char[]>() << std::endl;
+
+  std::cout << std::is_scalar<char[]>() << std::endl;
+  std::cout << std::is_scalar<char*>() << std::endl;
+
+
+
+
+  return TEST_RETURN_RESULT;
+}
