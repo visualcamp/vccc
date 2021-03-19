58ad6eb7b96e99c2bfca67d803514468689991ac
Tony <35574936+lackhole@users.noreply.github.com>
2021-03-19 10:51:11 +0900

    fix opencv features

--- type_support/include/vccc/type_support/cast.hpp
+++ type_support/include/vccc/type_support/cast.hpp
@@ -6,19 +6,53 @@
 # define VCCC_TYPE_SUPPORT_CAST_HPP_
 #
 # include "vccc/type_support/library_config.hpp"
+# include "vccc/type_traits.hpp"
 # include <cstddef>
 
 namespace vccc {
+
+namespace internal { namespace type_support {
+
+template<typename To, typename From, typename = void>
+struct saturate_cast_possible :
+# if VCCC_USE_OPENCV_FEATURES
+  public std::integral_constant<bool, are_arithmetic_v<To, From>>
+# else
+  public std::false_type
+# endif
+        {};
+
+template<typename To, typename From>
+using saturate_cast_possible_t = typename saturate_cast_possible<To, From>::type;
+
+template<typename To, typename From>
+constexpr inline
+decltype(auto)
+cast(std::false_type, From&& from) {
+  return static_cast<To>(std::forward<From>(from));
+}
+
+template<typename To, typename From>
+constexpr inline
+decltype(auto)
+cast(std::true_type, From from) {
+# if VCCC_USE_OPENCV_FEATURES
+  return cv::saturate_cast<To>(from);
+# else
+  cast(std::false_type{}, from);
+# endif
+}
+
+
+}}
 
 template<typename ToType, typename FromType>
 constexpr inline
 decltype(auto)
 cast(FromType&& from) {
-# if VCCC_USE_OPENCV_FEATURES
-  return cv::saturate_cast<ToType>(std::forward<FromType>(from));
-# else
-  return static_cast<ToType>(std::forward<FromType>(from));
-# endif
+  return internal::type_support::cast<ToType>(
+      internal::type_support::saturate_cast_possible_t<ToType, FromType>{},
+      std::forward<FromType>(from));
 }
 
 }
