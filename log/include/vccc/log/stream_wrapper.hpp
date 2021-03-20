789b7f8156480f39740ecbe66bab919e49bb901f
Tony <35574936+lackhole@users.noreply.github.com>
2021-03-20 22:01:27 +0900

    change std::chrono::steady_clock output

--- log/include/vccc/log/stream_wrapper.hpp
+++ log/include/vccc/log/stream_wrapper.hpp
@@ -12,10 +12,20 @@
 # include <tuple>
 # include <utility>
 # include <string>
+# include <ratio>
 # include <functional>
+# include "vccc/log/ios_flags_saver.hpp"
 # include "vccc/type_traits.hpp"
 
+//! @addtogroup log
+//! @{
+
 namespace vccc {
+
+/**
+@brief stream wrapper that supports various operator overloading
+
+ */
 
 template<typename Stream>
 class StreamWrapper {
@@ -28,12 +38,13 @@ class StreamWrapper {
 
   // for std::endl
   inline StreamWrapper&
-  operator << (std::ostream& (*pf)(std::ostream&)) { pf(stream_); return *this; }
+  operator << (std::ostream& (*pf)(std::ostream&))
+  { pf(stream_); return *this; }
 
-  // default printable type
   template<typename T>
   inline StreamWrapper&
-  operator << (const T& value) { try_write(typename is_printable<T>::type{}, value); return *this; }
+  operator << (const T& value)
+  { try_write(typename is_printable<T>::type{}, value); return *this; }
 
   inline stream_type& stream() { return stream_; }
   inline const stream_type& stream() const { return stream_; }
@@ -62,13 +73,19 @@ class StreamWrapper {
 
   // container types
   template<typename T, VCCC_ENABLE_IF_FORWARD(is_container_v<T>)>
-  void write(const T& value);
+  inline void write(const T& value) {
+    use_key_value_separator?
+      writeContainer(is_key_value_container_t<T>{}, value):
+      writeContainer(not_key_value_container_t{}, value);
+  }
 
   template<typename T>
-  void writeContainer(key_value_container_t, const T& value);
+  inline void writeContainer(key_value_container_t, const T& value)
+  { writeIterator(value.begin(), value.end(), [this](const auto& p){write(p, " : ");}); }
 
   template<typename T>
-  void writeContainer(not_key_value_container_t, const T& value);
+  inline void writeContainer(not_key_value_container_t, const T& value)
+  { writeIterator(value.begin(), value.end(), [this](const auto& elem){*this << elem;}); }
 
   template<typename InputIterator, typename Func>
   void writeIterator(InputIterator first, InputIterator last, Func f);
@@ -82,20 +99,24 @@ class StreamWrapper {
   void write(const std::chrono::time_point<std::chrono::system_clock, Duration>& time_point);
 
   template<typename Clock, typename Duration>
-  void write(const std::chrono::time_point<Clock, Duration>& time_point);
+  inline void write(const std::chrono::time_point<Clock, Duration>& time_point)
+  { stream_ << "+"; write(time_point.time_since_epoch());}
 
   void write(const std::time_t* tt);
 
 
   // tuple-like types
   template<typename ...Ts>
-  inline void write(const std::tuple<Ts...>& value) { writeTuple(value, std::index_sequence_for<Ts...>{});}
+  inline void write(const std::tuple<Ts...>& value)
+  { writeTuple(value, std::index_sequence_for<Ts...>{});}
 
   template<typename ...Ts>
-  inline void writeTuple(const std::tuple<Ts...>& value, std::index_sequence<>) { stream_ << "{}"; }
+  inline void writeTuple(const std::tuple<Ts...>& value, std::index_sequence<>)
+  { stream_ << "{}"; }
 
   template<typename ...Ts>
-  inline void writeTuple(const std::tuple<Ts...>& value, std::index_sequence<0>) { stream_ << "{ 0 }"; }
+  inline void writeTuple(const std::tuple<Ts...>& value, std::index_sequence<0>)
+  { stream_ << "{ " << std::get<0>(value) << " }"; }
 
   template<typename ...Ts, std::size_t ...I>
   void writeTuple(const std::tuple<Ts...>& value, std::index_sequence<0, I...>);
@@ -108,27 +129,6 @@ class StreamWrapper {
   template<typename T, T ...v>
   inline void write(const std::integer_sequence<T, v...>&) { write(std::make_tuple(v...)); }
 };
-
-template<typename Stream>
-template<typename T, VCCC_ENABLE_IF_IMPL(is_container_v<T>)>
-void StreamWrapper<Stream>::write(const T& value) {
-  if(use_key_value_separator)
-    writeContainer(is_key_value_container_t<T>{}, value);
-  else
-    writeContainer(not_key_value_container_t{}, value);
-}
-
-template<typename Stream>
-template<typename T>
-void StreamWrapper<Stream>::writeContainer(StreamWrapper::key_value_container_t, const T& value) {
-  writeIterator(value.begin(), value.end(), [this](const auto& p){write(p, " : ");});
-}
-
-template<typename Stream>
-template<typename T>
-void StreamWrapper<Stream>::writeContainer(StreamWrapper::not_key_value_container_t, const T& value) {
-  writeIterator(value.begin(), value.end(), [this](const auto& elem){*this << elem;});
-}
 
 template<typename Stream>
 template<typename InputIterator, typename Func>
@@ -167,19 +167,34 @@ void StreamWrapper<Stream>::write(const std::chrono::duration<Rep, Period>& dura
     }
   }
   else {
-    auto mins = std::chrono::duration_cast<std::chrono::minutes>(duration);
-    auto secs = std::chrono::duration_cast<std::chrono::seconds>(duration);
+    using days    = std::chrono::duration<long, std::ratio<   86400>>;
+    using months  = std::chrono::duration<long, std::ratio< 2629746>>;
+    using years   = std::chrono::duration<long, std::ratio<31556952>>;
 
-    if(duration < std::chrono::minutes(60)) {
-      stream_ << mins.count() << "m "
-              << (secs-mins).count() << "s";
+    auto hh = std::chrono::duration_cast<std::chrono::hours       >(duration % days                (1)).count();
+    auto mm = std::chrono::duration_cast<std::chrono::minutes     >(duration % std::chrono::hours  (1)).count();
+    auto ss = std::chrono::duration_cast<std::chrono::seconds     >(duration % std::chrono::minutes(1)).count();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration % std::chrono::seconds(1)).count();
+
+    if(duration > days(1)) {
+      auto DD = std::chrono::duration_cast<days  >(duration % months(1)).count();
+      auto MM = std::chrono::duration_cast<months>(duration % years(1)).count();
+      auto YY = std::chrono::duration_cast<years >(duration).count();
+
+      if(YY) stream_ << YY << "Y ";
+      if(MM) stream_ << MM << "M ";
+      if(DD) stream_ << DD << "D ";
     }
-    else {
-      auto hrs = std::chrono::duration_cast<std::chrono::hours>(duration);
-      stream_ << hrs.count() << "h "
-              << (mins - hrs).count() << "m "
-              << (secs - mins).count() << "s";
-    }
+
+    IOSFlagsSaver<stream_type> saver(stream_);
+    if(duration >= std::chrono::hours(1))
+      stream_ << std::setw(2) << std::setfill(' ') << hh << "h "
+              << std::setw(2) << std::setfill(' ') << mm << "m ";
+    else if(duration >= std::chrono::minutes(1))
+      stream_ << std::setw(2) << std::setfill(' ') << mm << "m ";
+
+    stream_ << std::setw(2) << std::setfill(' ') << ss << '.';
+    stream_ << std::setw(3) << std::setfill('0') << ms << 's';
   }
 }
 
@@ -193,31 +208,19 @@ void StreamWrapper<Stream>::write(const std::chrono::time_point<std::chrono::sys
   std::unique_lock<std::mutex> lck(localtime_m);
   const std::tm* tm_obj = std::localtime(&tt);
 
-  if(tm_obj == nullptr) // failed parsing
-    return std::to_string(tt);
+  if(tm_obj == nullptr){// failed parsing
+    std::to_string(tt);
+    return;
+  }
 
   stream_ << std::put_time(tm_obj, "%Y-%m-%d %H:%M:%S.");
   lck.unlock();
 
   // calculate milliseconds
-  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(time_point.time_since_epoch())
-      - std::chrono::duration_cast<std::chrono::seconds>(time_point.time_since_epoch());
-  stream_ << std::setfill('0') << std::setw(3) << ms.count();
-}
-
-template<typename Stream>
-template<typename Clock, typename Duration>
-void StreamWrapper<Stream>::write(const std::chrono::time_point<Clock, Duration>& time_point) {
-  using system_clock = std::chrono::system_clock;
-
-  // TODO: use custom steady_clock output instead of converting to system_clock
-  static const auto system_clock_begin = system_clock::now();
-  static const auto custom_clock_begin = Clock::now();
-  static const auto gap =
-      system_clock_begin.time_since_epoch() - custom_clock_begin.time_since_epoch();
-
-  auto d = std::chrono::duration_cast<std::chrono::system_clock::duration>(time_point.time_since_epoch() + gap);
-  write(std::chrono::time_point<std::chrono::system_clock>() + d);
+  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+      time_point.time_since_epoch() % std::chrono::seconds(1)).count();
+  IOSFlagsSaver<stream_type> saver(stream_);
+  stream_ << std::setfill('0') << std::setw(3) << ms;
 }
 
 template<typename Stream>
@@ -255,5 +258,7 @@ void StreamWrapper<Stream>::write(const std::pair<T1, T2>& value, const std::str
 }
 
 } // namespace vccc
+
+//! @} log
 
 # endif //VCCC_LOG_STREAM_WRAPPER_HPP_
