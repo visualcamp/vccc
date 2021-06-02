06c2a20949b948b43e90d403d6b7111b8ae9439e
Tony <cosgenio@gmail.com>
2021-06-03 00:27:21 +0900

    add test

--- experimental/test/signal_test.cc
+++ experimental/test/signal_test.cc
@@ -2,6 +2,7 @@
 // Created by YongGyu Lee on 2021/06/03.
 //
 
+#include <atomic>
 #include <iostream>
 
 #include "vccc/experimental/signal.hpp"
@@ -10,7 +11,15 @@
 int main() {
   INIT_TEST("vccc::experimental::signal");
 
-  vccc::experimental::signal::signal<void()> signal;
+  {
+    vccc::experimental::signal::signal<void()> signal;
+
+    std::atomic_int sum{0};
+    for(int i=0; i<100; ++i)
+      signal.connect([&](){++sum;});
+    signal();
+    TEST_ENSURES(sum == 100);
+  }
 
   return TEST_RETURN_RESULT;
 }
