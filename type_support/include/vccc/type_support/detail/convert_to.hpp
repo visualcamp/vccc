6b8292b838c662d9046a19cc11deb05b725b594a
Tony <cosgenio@gmail.com>
2022-02-01 22:44:46 +0900

    Add size check

--- type_support/include/vccc/type_support/detail/convert_to.hpp
+++ type_support/include/vccc/type_support/detail/convert_to.hpp
@@ -15,12 +15,21 @@
 namespace vccc{
 namespace detail{
 
+template<typename T, typename = void>
+struct has_size : std::false_type {};
+
+template<typename T>
+struct has_size<T, void_t<decltype(std::declval<T&>().size())>> : std::true_type {};
+
+template<size_t sz, typename T> inline bool check_bound(std::true_type, const T& from) { return from.size() >= sz; }
+template<size_t sz, typename T> inline bool check_bound(std::false_type, const T&) { return true; }
+
 template<typename T, bool v = is_tuple_like<T>::value>
 struct tuple_size_or_zero : std::integral_constant<size_t, 0> {};
 
 template<typename T>
 struct tuple_size_or_zero<T, true> : std::tuple_size<T> {};
-          
+
 
 // fill rest with integer_sequence
 template<typename R, typename T, std::size_t... I, typename IT, IT... I2>
@@ -34,7 +43,9 @@ fill_rest(const T& from, std::index_sequence<I...> index_seq, std::integer_seque
 template<typename R, typename T, std::size_t... I>
 inline R
 convert_to_impl(std::true_type, const T& from, std::index_sequence<I...> index_seq)
-{  
+{
+  ENSURES(check_bound<std::tuple_size<R>::value>(bool_constant<!is_tuple_like<T>::value && has_size<T>::value>{}, from),
+          "Following must be satisfied: from.size() >= tuple_size<R>::value");
   using Zeros = make_zero_sequence<(
     (tuple_size_or_zero<R>::value > tuple_size_or_zero<T>::value && is_tuple_like<T>::value)
         ? static_diff<size_t, tuple_size_or_zero<R>::value, tuple_size_or_zero<T>::value>::value : 0)>;
