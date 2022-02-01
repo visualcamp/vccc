4f968e8aca0e3fe753111b5266c0d00da56235da
Tony <cosgenio@gmail.com>
2022-02-01 22:06:24 +0900

    Create is_tuple_like.h

---
+++ type_traits/include/vccc/type_traits/is_tuple_like.h
@@ -0,0 +1,21 @@
+# /*
+#  * Created by YongGyu Lee on 2022/02/01.
+#  */
+#
+# ifndef VCCC_TYPE_TRAITS_IS_TUPLE_LIKE_H_
+# define VCCC_TYPE_TRAITS_IS_TUPLE_LIKE_H_
+# 
+# include <tuple>
+# include <type_traits>
+
+namespace vccc {
+
+template<typename T, typename = void>
+struct is_tuple_like : std::false_type {};
+
+template<typename T>
+struct is_tuple_like<T, void_t<decltype(std::tuple_size<T>::value)>> : std::true_type {};
+
+} // namespace vccc
+
+# endif // VCCC_TYPE_TRAITS_IS_TUPLE_LIKE_H_
