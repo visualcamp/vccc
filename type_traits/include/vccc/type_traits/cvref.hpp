402385d29022deba56492fceeb1d82faa2de7d77
lackhole <cosgenio@gmail.com>
2023-03-31 16:13:05 +0900

    Add remove_cvref

---
+++ type_traits/include/vccc/type_traits/cvref.hpp
@@ -0,0 +1,17 @@
+# /*
+#  * Created by YongGyu Lee on 2020/12/08.
+#  */
+#
+# ifndef VCCC_TYPE_TRAITS_CVREF_HPP
+# define VCCC_TYPE_TRAITS_CVREF_HPP
+#
+# include <type_traits>
+
+namespace vccc{
+
+template<typename T> struct remove_cvref { using type = std::remove_cv_t<std::remove_reference_t<T>>; };
+template<typename T> using remove_cvref_t = typename remove_cvref<T>::type;
+
+} // namespace vccc
+
+# endif // VCCC_TYPE_TRAITS_CVREF_HPP
