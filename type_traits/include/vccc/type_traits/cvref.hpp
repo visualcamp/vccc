51778a0375484e0c3f28fb552243808eb48c763d
lackhole <cosgenio@gmail.com>
2023-03-31 16:58:56 +0900

    Add doxygen docs

--- type_traits/include/vccc/type_traits/cvref.hpp
+++ type_traits/include/vccc/type_traits/cvref.hpp
@@ -9,8 +9,21 @@
 
 namespace vccc{
 
+/**
+@addtogroup type_traits
+@{
+    @defgroup remove_cvref remove_cvref
+    @brief combines std::remove_cv and std::remove_reference
+@}
+
+@addtogroup remove_cvref
+@{
+*/
+
 template<typename T> struct remove_cvref { using type = std::remove_cv_t<std::remove_reference_t<T>>; };
 template<typename T> using remove_cvref_t = typename remove_cvref<T>::type;
+
+//! @} remove_cvref
 
 } // namespace vccc
 
