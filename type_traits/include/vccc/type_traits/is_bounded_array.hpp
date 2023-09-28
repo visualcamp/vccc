85ba8c33d044871b28343b94456cf4b6dad25e2e
Yong Gyu Lee <cosgenio@gmail.com>
2023-09-29 05:08:15 +0900

    Implement C++17, C++20 type_traits features

---
+++ type_traits/include/vccc/type_traits/is_bounded_array.hpp
@@ -0,0 +1,21 @@
+//
+// Created by yonggyulee on 2023/09/29.
+//
+
+#ifndef VCCC_TYPE_TRAITS_IS_BOUNDED_ARRAY_HPP_
+#define VCCC_TYPE_TRAITS_IS_BOUNDED_ARRAY_HPP_
+
+#include <cstddef>
+#include <type_traits>
+
+namespace vccc {
+
+template<class T>
+struct is_bounded_array : std::false_type {};
+
+template<class T, std::size_t N>
+struct is_bounded_array<T[N]> : std::true_type {};
+
+} // namespace vccc
+
+#endif // VCCC_TYPE_TRAITS_IS_BOUNDED_ARRAY_HPP_
