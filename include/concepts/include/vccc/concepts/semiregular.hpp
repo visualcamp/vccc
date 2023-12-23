47bc868656624f5661dd343ae693414268c91c72
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 02:24:42 +0900

    Create semiregular.hpp

---
+++ include/concepts/include/vccc/concepts/semiregular.hpp
@@ -0,0 +1,30 @@
+//
+// Created by yonggyulee on 2023/12/24.
+//
+
+#ifndef VCCC_CONCEPTS_SEMIREGULAR_HPP_
+#define VCCC_CONCEPTS_SEMIREGULAR_HPP_
+
+#include "vccc/concepts/copyable.hpp"
+#include "vccc/concepts/default_initializable.hpp"
+#include "vccc/type_traits/conjunction.hpp"
+
+namespace vccc {
+namespace concepts {
+
+/// @addtogroup concepts
+/// @{
+
+template<typename T>
+struct semiregular
+    : conjunction<
+        copyable<T>,
+        default_initializable<T>
+      > {};
+
+/// @}
+
+} // namespace concepts
+} // namespace vccc
+
+#endif // VCCC_CONCEPTS_SEMIREGULAR_HPP_
