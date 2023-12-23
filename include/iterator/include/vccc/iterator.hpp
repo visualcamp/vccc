5165fe6f733325b3ff4164e8b028a53e45ac6120
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-23 15:06:27 +0900

    Create iterator.hpp

---
+++ include/iterator/include/vccc/iterator.hpp
@@ -0,0 +1,20 @@
+//
+// Created by yonggyulee on 2023/12/23.
+//
+
+#ifndef VCCC_ITERATOR_HPP_
+#define VCCC_ITERATOR_HPP_
+
+#include "vccc/iterator/incrementable_traits.hpp"
+#include "vccc/iterator/input_iterator.hpp"
+#include "vccc/iterator/input_or_output_iterator.hpp"
+#include "vccc/iterator/iter_difference_t.hpp"
+#include "vccc/iterator/iter_reference_t.hpp"
+#include "vccc/iterator/weakly_incrementable.hpp"
+
+/**
+@defgroup iterator iterator
+@brief Iterators
+*/
+
+#endif // VCCC_ITERATOR_HPP_
