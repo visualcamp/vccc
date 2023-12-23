99415eba9e4e6b982235cd531625118a293fd9b3
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 02:45:21 +0900

    Fix dereferenceable

--- include/concepts/include/vccc/concepts/dereferenceable.hpp
+++ include/concepts/include/vccc/concepts/dereferenceable.hpp
@@ -7,17 +7,19 @@
 
 #include <type_traits>
 
+#include "vccc/type_traits/is_referenceable.hpp"
 #include "vccc/type_traits/void_t.hpp"
 
 namespace vccc {
 namespace concepts {
 namespace detail {
 
-template<typename T, typename = void>
+template<typename T, bool = is_referencable<T>::value, typename = void>
 struct dereferenceable_impl : std::false_type {};
 
 template<typename T>
-struct dereferenceable_impl<T, void_t<decltype(*std::declval<T&>())>> : std::true_type {};
+struct dereferenceable_impl<T, true, void_t<decltype(*std::declval<T&>())>>
+    : is_referencable<decltype(*std::declval<T&>())> {};
 
 } // namespace detail
 
