6ef68af74a393d8b02c22471cf976d04c5c9bfb6
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 00:53:44 +0900

    Update swappable.hpp

--- include/concepts/include/vccc/concepts/swappable.hpp
+++ include/concepts/include/vccc/concepts/swappable.hpp
@@ -5,12 +5,32 @@
 #ifndef VCCC_CONCEPTS_SWAPPABLE_HPP_
 #define VCCC_CONCEPTS_SWAPPABLE_HPP_
 
-#include "vccc/concepts/ranges/swap.hpp"
+#include <type_traits>
+
+#include "vccc/ranges/swap.hpp"
+#include "vccc/type_traits/is_referenceable.hpp"
+#include "vccc/type_traits/void_t.hpp"
 
 namespace vccc {
 namespace concepts {
+namespace detail {
 
+template<typename T, bool = is_referencable<T>::value, typename = void>
+struct swappable_impl : std::false_type {};
 
+template<typename T>
+struct swappable_impl<T, true, void_t<decltype(ranges::swap(std::declval<T&>(), std::declval<T&>()))>>
+    : std::true_type {};
+
+} // namespace detail
+
+/// @addtogroup concepts
+/// @{
+
+template<typename T>
+struct swappable : detail::swappable_impl<T> {};
+
+/// @}
 
 } // namespace concepts
 } // namespace vccc
