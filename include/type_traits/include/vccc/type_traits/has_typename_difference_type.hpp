38863a524275dd7de2629040b0bb3091fc8b366b
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 01:54:02 +0900

    Create has_typename_difference_type.hpp

---
+++ include/type_traits/include/vccc/type_traits/has_typename_difference_type.hpp
@@ -0,0 +1,22 @@
+//
+// Created by yonggyulee on 2023/12/24.
+//
+
+#ifndef VCCC_TYPE_TRAITS_HAS_TYPENAME_DIFFERENCE_TYPE_HPP_
+#define VCCC_TYPE_TRAITS_HAS_TYPENAME_DIFFERENCE_TYPE_HPP_
+
+#include <type_traits>
+
+#include "vccc/type_traits/void_t.hpp"
+
+namespace vccc {
+
+template<typename T, typename = void>
+struct has_typename_difference_type : std::false_type {};
+
+template<typename T>
+struct has_typename_difference_type<T, void_t<typename T::difference_type>> : std::true_type {};
+
+} // namespace vccc
+
+#endif // VCCC_TYPE_TRAITS_HAS_TYPENAME_DIFFERENCE_TYPE_HPP_
