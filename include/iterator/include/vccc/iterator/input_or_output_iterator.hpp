df9ae7c2a0631d0eaf843c928591cca2cb9fb155
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 02:45:42 +0900

    Implement input_or_output_iterator

--- include/iterator/include/vccc/iterator/input_or_output_iterator.hpp
+++ include/iterator/include/vccc/iterator/input_or_output_iterator.hpp
@@ -5,12 +5,21 @@
 #ifndef VCCC_ITERATOR_INPUT_OR_OUTPUT_ITERATOR_HPP_
 #define VCCC_ITERATOR_INPUT_OR_OUTPUT_ITERATOR_HPP_
 
+#include "vccc/concepts/dereferenceable.hpp"
 #include "vccc/iterator/weakly_incrementable.hpp"
+#include "vccc/type_traits/conjunction.hpp"
 
 namespace vccc {
+namespace concepts {
 
+template<typename I>
+struct input_or_output_iterator
+    : conjunction<
+        dereferenceable<I>,
+        weakly_incrementable<I>
+      > {};
 
-
+} // namespace concepts
 } // namespace vccc
 
 #endif // VCCC_ITERATOR_INPUT_OR_OUTPUT_ITERATOR_HPP_
