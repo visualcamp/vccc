5f679fe7f74406b272630007b838b9aefdf2ec3c
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-24 14:52:00 +0900

    Create sentinel_for.hpp

---
+++ include/iterator/include/vccc/iterator/sentinel_for.hpp
@@ -0,0 +1,27 @@
+//
+// Created by yonggyulee on 2023/12/24.
+//
+
+#ifndef VCCC_ITERATOR_SENTINEL_FOR_HPP_
+#define VCCC_ITERATOR_SENTINEL_FOR_HPP_
+
+#include "vccc/concepts/semiregular.hpp"
+#include "vccc/concepts/weakly_equality_comparable_with.hpp"
+#include "vccc/iterator/input_or_output_iterator.hpp"
+#include "vccc/type_traits/conjunction.hpp"
+
+namespace vccc {
+namespace concepts {
+
+template<typename S, typename I>
+struct sentinel_for
+    : conjunction<
+        semiregular<S>,
+        input_or_output_iterator<I>,
+        weakly_equality_comparable_with<S, I>
+      > {};
+
+} // namespace vccc
+} // namespace concepts
+
+#endif // VCCC_ITERATOR_SENTINEL_FOR_HPP_
