214043a461a020db318af02b6e99f4bd584b1966
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-25 15:39:08 +0900

    Fix copyable

--- include/concepts/include/vccc/concepts/copyable.hpp
+++ include/concepts/include/vccc/concepts/copyable.hpp
@@ -5,16 +5,23 @@
 #ifndef VCCC_CONCEPTS_COPYABLE_HPP_
 #define VCCC_CONCEPTS_COPYABLE_HPP_
 
+#include <type_traits>
+
 #include "vccc/concepts/assignable_from.hpp"
 #include "vccc/concepts/copy_constructible.hpp"
 #include "vccc/concepts/movable.hpp"
 #include "vccc/type_traits/conjunction.hpp"
+#include "vccc/type_traits/is_referenceable.hpp"
 
 namespace vccc {
 namespace concepts {
+namespace detail {
+
+template<typename T, bool = is_referencable<T>::value>
+struct copyable_impl : std::false_type {};
 
 template<typename T>
-struct copyable
+struct copyable_impl<T, true>
     : conjunction<
         copy_constructible<T>,
         movable<T>,
@@ -22,6 +29,16 @@ struct copyable
         assignable_from<T&, const T&>,
         assignable_from<T&, const T>
       > {};
+
+} // namespace detail
+
+/// @addtogroup concepts
+/// @{
+
+template<typename T>
+struct copyable : detail::copyable_impl<T> {};
+
+/// @}
 
 } // namespace concepts
 } // namespace vccc
