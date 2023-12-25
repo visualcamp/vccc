ad427c3dc847617afa76670c148fdaab9206b7f5
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-25 15:39:42 +0900

    Create regular.hpp

---
+++ include/concepts/include/vccc/concepts/regular.hpp
@@ -0,0 +1,30 @@
+//
+// Created by yonggyulee on 2023/12/24.
+//
+
+#ifndef VCCC_CONCEPTS_REGULAR_HPP_
+#define VCCC_CONCEPTS_REGULAR_HPP_
+
+#include "vccc/concepts/semiregular.hpp"
+#include "vccc/concepts/equality_comparable.hpp"
+#include "vccc/type_traits/conjunction.hpp"
+
+namespace vccc {
+namespace concepts {
+
+/// @addtogroup concepts
+/// @{
+
+template<typename T>
+struct regular
+    : conjunction<
+        semiregular<T>,
+        equality_comparable<T>
+      > {};
+
+/// @}
+
+} // namespace concepts
+} // namespace vccc
+
+#endif // VCCC_CONCEPTS_REGULAR_HPP_
