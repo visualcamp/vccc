553580e2dc137e498b91abee799882831d3e6f2e
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-25 15:39:02 +0900

    Fix copy_constructible

--- include/concepts/include/vccc/concepts/copy_constructible.hpp
+++ include/concepts/include/vccc/concepts/copy_constructible.hpp
@@ -12,15 +12,29 @@
 
 namespace vccc {
 namespace concepts {
+namespace detail {
+
+template<typename T, bool = is_referencable<T>::value>
+struct copy_constructible_impl : std::false_type {};
 
 template<typename T>
-struct copy_constructible
+struct copy_constructible_impl<T, true>
     : conjunction<
         move_constructible<T>,
         constructible_from<T, T&>, convertible_to<T&, T>,
         constructible_from<T, const T&>, convertible_to<const T&, T>,
         constructible_from<T, const T>, convertible_to<const T, T>
       > {};
+
+} // namespace detail
+
+/// @addtogroup concepts
+/// @{
+
+template<typename T>
+struct copy_constructible : detail::copy_constructible_impl<T> {};
+
+/// @}
 
 } // namespace concepts
 } // namespace vccc
