f1b0a49e3f5a0bd2f589d970a9f2dfee9bb7f7ae
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-25 15:39:26 +0900

    Fix movable

--- include/concepts/include/vccc/concepts/movable.hpp
+++ include/concepts/include/vccc/concepts/movable.hpp
@@ -12,20 +12,31 @@
 #include "vccc/concepts/swappable.hpp"
 #include "vccc/type_traits/conjunction.hpp"
 #include "vccc/type_traits/is_swappable.hpp"
+#include "vccc/type_traits/is_referenceable.hpp"
 
 namespace vccc {
 namespace concepts {
+namespace detail {
+
+template<typename T, bool = is_referencable<T>::value>
+struct movable_impl : std::false_type {};
+
+template<typename T>
+struct movable_impl<T, true>
+    : conjunction<
+        std::is_object<T>,
+        move_constructible<T>,
+        assignable_from<T&, T>,
+        swappable<T>
+      > {};
+
+} // namespace detail
 
 /// @addtogroup concepts
 /// @{
 
 template<typename T>
-struct movable : conjunction<
-    std::is_object<T>,
-    move_constructible<T>,
-    assignable_from<T&, T>,
-    swappable<T>
-    > {};
+struct movable : detail::movable_impl<T> {};
 
 /// @}
 
