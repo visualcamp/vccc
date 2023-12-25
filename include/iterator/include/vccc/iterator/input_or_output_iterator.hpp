ae9ff7987043da0517a71607f1aa1f3d9db23d97
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-25 18:05:13 +0900

    Update input_or_output_iterator.hpp

--- include/iterator/include/vccc/iterator/input_or_output_iterator.hpp
+++ include/iterator/include/vccc/iterator/input_or_output_iterator.hpp
@@ -12,12 +12,38 @@
 namespace vccc {
 namespace concepts {
 
+/// @addtogroup iterator
+/// @{
+
+
+/**
+@brief
+specifies that objects of a type can be incremented and dereferenced
+
+@code{.cpp}
 template<typename I>
 struct input_or_output_iterator
     : conjunction<
         dereferenceable<I>,
         weakly_incrementable<I>
       > {};
+@endcode
+
+The `%input_or_output_iterator` concept forms the basis of the iterator concept taxonomy;
+every iterator type satisfies the `%input_or_output_iterator` requirements.
+
+@sa [std::input_or_output_iterator](https://en.cppreference.com/w/cpp/iterator/input_or_output_iterator)
+
+
+ */
+template<typename I>
+struct input_or_output_iterator
+    : conjunction<
+        dereferenceable<I>,
+        weakly_incrementable<I>
+      > {};
+
+/// @}
 
 } // namespace concepts
 } // namespace vccc
