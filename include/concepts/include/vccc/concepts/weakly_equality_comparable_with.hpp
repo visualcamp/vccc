88630e0186cdba095613c2c24c6721c90a4b00e8
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-26 05:09:10 +0900

    Update weakly_equality_comparable_with.hpp

--- include/concepts/include/vccc/concepts/weakly_equality_comparable_with.hpp
+++ include/concepts/include/vccc/concepts/weakly_equality_comparable_with.hpp
@@ -6,6 +6,7 @@
 #define VCCC_CONCEPTS_WEAKLY_EQUALITY_COMPARABLE_WITH_HPP_
 
 #include <type_traits>
+#include <utility>
 
 #include "vccc/type_traits/conjunction.hpp"
 #include "vccc/type_traits/is_referenceable.hpp"
@@ -36,26 +37,26 @@ template<
 struct weakly_equality_comparable_with_impl : std::false_type {};
 
 template<typename T, typename U>
-struct weakly_equality_comparable_with_impl<T, U, true> {};
+struct weakly_equality_comparable_with_impl<T, U, true>
+    : conjunction<
+        is_explicitly_equality_comparable<
+          std::add_lvalue_reference_t<const std::remove_reference_t<T>>,
+          std::add_lvalue_reference_t<const std::remove_reference_t<U>>>,
+        is_explicitly_equality_comparable<
+          std::add_lvalue_reference_t<const std::remove_reference_t<U>>,
+          std::add_lvalue_reference_t<const std::remove_reference_t<T>>>,
+        is_explicitly_non_equality_comparable<
+          std::add_lvalue_reference_t<const std::remove_reference_t<T>>,
+          std::add_lvalue_reference_t<const std::remove_reference_t<U>>>,
+        is_explicitly_non_equality_comparable<
+          std::add_lvalue_reference_t<const std::remove_reference_t<U>>,
+          std::add_lvalue_reference_t<const std::remove_reference_t<T>>>
+      > {};
 
 } // namespace detail
 
 template<typename T, typename U>
-struct weakly_equality_comparable_with
-    : conjunction<
-        detail::is_explicitly_equality_comparable<
-          std::add_lvalue_reference_t<const std::remove_reference_t<T>>,
-          std::add_lvalue_reference_t<const std::remove_reference_t<U>>>,
-        detail::is_explicitly_equality_comparable<
-          std::add_lvalue_reference_t<const std::remove_reference_t<U>>,
-          std::add_lvalue_reference_t<const std::remove_reference_t<T>>>,
-        detail::is_explicitly_non_equality_comparable<
-          std::add_lvalue_reference_t<const std::remove_reference_t<T>>,
-          std::add_lvalue_reference_t<const std::remove_reference_t<U>>>,
-        detail::is_explicitly_non_equality_comparable<
-          std::add_lvalue_reference_t<const std::remove_reference_t<U>>,
-          std::add_lvalue_reference_t<const std::remove_reference_t<T>>>
-      > {};
+struct weakly_equality_comparable_with : detail::weakly_equality_comparable_with_impl<T, U> {};
 
 } // namespace vccc
 
