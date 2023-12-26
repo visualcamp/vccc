b44f91d83ea20f3be3da9740ab8d3950e0d57f8c
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-27 04:56:36 +0900

    Update hash_array.hpp

--- include/functional/include/vccc/functional/hash_array.hpp
+++ include/functional/include/vccc/functional/hash_array.hpp
@@ -14,14 +14,30 @@ namespace vccc {
 //! @addtogroup functional
 //! @{
 
-template<typename T, std::size_t Bytes = sizeof(T) * CHAR_BIT> struct basic_FNV_prime;
-template<typename T, std::size_t Bytes = sizeof(T) * CHAR_BIT> struct basic_FNV_offset_basis;
+/// @addtogroup functional_FNV_prime__class basic_FNV_prime
+/// @{
+template<typename T, std::size_t Bytes = sizeof(T) * CHAR_BIT>
+struct basic_FNV_prime;
+template<typename T>
+struct basic_FNV_prime<T, 32> : std::integral_constant<T, 0x01000193> {};
+template<typename T>
+struct basic_FNV_prime<T, 64> : std::integral_constant<T, 0x00000100000001B3> {};
 
-template<typename T> struct basic_FNV_prime<T, 32> : std::integral_constant<T, 0x01000193> {};
-template<typename T> struct basic_FNV_offset_basis<T, 32> : std::integral_constant<T, 0x811c9dc5> {};
+using FNV_prime = basic_FNV_prime<std::size_t>;
+/// @} functional_FNV_prime__class
 
-template<typename T> struct basic_FNV_prime<T, 64> : std::integral_constant<T, 0x00000100000001B3> {};
-template<typename T> struct basic_FNV_offset_basis<T, 64> : std::integral_constant<T, 0xcbf29ce484222325> {};
+
+/// @addtogroup functional_FNV_offset_basis__class basic_FNV_offset_basis
+/// @{
+template<typename T, std::size_t Bytes = sizeof(T) * CHAR_BIT>
+struct basic_FNV_offset_basis;
+template<typename T>
+struct basic_FNV_offset_basis<T, 32> : std::integral_constant<T, 0x811c9dc5> {};
+template<typename T>
+struct basic_FNV_offset_basis<T, 64> : std::integral_constant<T, 0xcbf29ce484222325> {};
+
+using FNV_offset_basis = basic_FNV_offset_basis<std::size_t>;
+/// @} functional_FNV_offset_basis__class
 
 // template<typename T> struct basic_FNV_prime<T, 128> : std::integral_constant<T, 0x0000000001000000000000000000013B> {};
 // template<typename T> struct basic_FNV_offset_basis<T, 128> : std::integral_constant<T, 0x6c62272e07bb014262b821756295c58d> {};
@@ -31,9 +47,12 @@ template<typename T> struct basic_FNV_offset_basis<T, 64> : std::integral_consta
 // template<typename T> struct basic_FNV_offset_basis<T, 256>
 //     : std::integral_constant<T, 0xdd268dbcaac550362d98c384c4e576ccc8b1536847b6bbb31023b4c8caee0535> {};
 
-using FNV_prime = basic_FNV_prime<std::size_t>;
-using FNV_offset_basis = basic_FNV_offset_basis<std::size_t>;
 
+/// @addtogroup functional_fnv1__func__fnv1 FNV_1, FNV_1a
+/// @brief FNV hash algorithm
+///
+/// [Fowler–Noll–Vo hash function](https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
+/// @{
 template<typename T>
 std::size_t FNV_1(std::size_t value, const T& byte) {
   static_assert(std::is_integral<T>::value, "T must integral type");
@@ -63,28 +82,24 @@ inline std::size_t FNV_1a(std::size_t value, const unsigned char* const bytes, s
   }
   return value;
 }
+/// @} functional_fnv1__func__fnv1
 
-/**
- * \brief Do hash operation on array
- *
- * FNV_1a is used to get hash value
- */
+/// @addtogroup functional_hash_array__func__hash_array hash_array
+/// @brief FNV Do hash operation on array
+/// @{
 template<typename T>
 std::size_t hash_array(const T* bytes, std::size_t size) {
   return FNV_1a(FNV_offset_basis::value, reinterpret_cast<const unsigned char* const>(bytes), sizeof(T) * size);
 }
 
-/**
- * \brief Do hash operation on array
- *
- * FNV_1a is used to get hash value
- */
+
 template<typename T, std::size_t N>
 std::size_t hash_array(const T(&bytes)[N]) {
   return hash_array(bytes, N);
 }
+/// @} functional_hash_array__func__hash_array
 
-//! @}
+//! @} functional
 
 } // namespace vccc
 
