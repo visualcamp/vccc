d927dbb53af39f2be1249b381e9dca71f0c14f3e
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-27 02:48:10 +0900

    Implement totally_ordered_with

--- include/concepts/include/vccc/concepts/totally_ordered.hpp
+++ include/concepts/include/vccc/concepts/totally_ordered.hpp
@@ -9,6 +9,7 @@
 
 #include "vccc/concepts/equality_comparable.hpp"
 #include "vccc/concepts/partially_ordered_with.hpp"
+#include "vccc/type_traits/common_reference.hpp"
 #include "vccc/type_traits/conjunction.hpp"
 
 namespace vccc {
@@ -18,11 +19,14 @@ namespace vccc {
 
 
 /**
- *  @brief specifies that the comparison operators on the type yield a total order
- *
- *  The concept `totally_ordered` specifies that the comparison operators `==`,`!=`,`<`,`>`,`<=`,`>=` on a type yield
- *  results consistent with a [strict total order](https://en.wikipedia.org/wiki/Total_order#Strict_and_non-strict_total_orders)
- *  on the type.
+@brief specifies that the comparison operators on the type yield a total order
+
+The concept `totally_ordered` specifies that the comparison operators `==`,`!=`,`<`,`>`,`<=`,`>=` on a type yield
+results consistent with a [strict total order](https://en.wikipedia.org/wiki/Total_order#Strict_and_non-strict_total_orders)
+on the type.
+
+@sa [std::totally_ordered](https://en.cppreference.com/w/cpp/concepts/totally_ordered)
+@sa totally_ordered_with
  */
 template<typename T>
 struct totally_ordered
@@ -30,6 +34,44 @@ struct totally_ordered
         equality_comparable<T>,
         partially_ordered_with<T, T>
       >{};
+
+
+namespace detail {
+
+template<
+    typename T,
+    typename U,
+    bool = conjunction<
+               totally_ordered<T>,
+               totally_ordered<U>,
+               equality_comparable_with<T, U>
+           >::value /* false */
+>
+struct totally_ordered_with_impl : std::false_type {};
+
+template<typename T, typename U>
+struct totally_ordered_with_impl<T, U, true>
+    : conjunction<
+        totally_ordered<
+            common_reference_t< const std::remove_reference_t<T>&,
+                                const std::remove_reference_t<U>&> >,
+        partially_ordered_with<T, U>
+      > {};
+
+} // namespace detail
+
+/**
+\brief \copybrief totally_ordered
+
+The concept `%vccc::totally_ordered_with` specifies that the comparison operators `==`,`!=`,`<`,`>`,`<=`,`>=` on
+(possibly mixed) `T` and `U` operands yield results consistent with a strict total order.
+Comparing mixed operands yields results equivalent to comparing the operands converted to their common type.
+
+@sa [std::totally_ordered_with](https://en.cppreference.com/w/cpp/concepts/totally_ordered)
+@sa totally_ordered
+ */
+template<typename T, typename U>
+struct totally_ordered_with : detail::totally_ordered_with_impl<T, U> {};
 
 /// @}
 
