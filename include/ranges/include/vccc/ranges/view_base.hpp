9a50f46d3dd2d24314912cffe1dc121404578d3f
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-30 00:08:05 +0900

    Create view_base.hpp

---
+++ include/ranges/include/vccc/ranges/view_base.hpp
@@ -0,0 +1,16 @@
+//
+// Created by yonggyulee on 2023/12/29.
+//
+
+#ifndef VCCC_RANGES_VIEW_BASE_HPP
+#define VCCC_RANGES_VIEW_BASE_HPP
+
+namespace vccc {
+namespace ranges {
+
+struct view_base {};
+
+} // namespace vccc
+} // namespace ranges
+
+#endif // VCCC_RANGES_VIEW_BASE_HPP
