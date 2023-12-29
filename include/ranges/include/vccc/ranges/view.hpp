05a8db4601f6385014bf1b882ec6678209955676
Yong Gyu Lee <cosgenio@gmail.com>
2023-12-30 03:58:11 +0900

    Update view.hpp

--- include/ranges/include/vccc/ranges/view.hpp
+++ include/ranges/include/vccc/ranges/view.hpp
@@ -5,11 +5,37 @@
 #ifndef VCCC_RANGES_VIEW_HPP_
 #define VCCC_RANGES_VIEW_HPP_
 
+#include <type_traits>
+
+#include "vccc/concepts/movable.hpp"
+#include "vccc/ranges/enable_view.hpp"
+#include "vccc/ranges/range.hpp"
+#include "vccc/type_traits/conjunction.hpp"
+
 namespace vccc {
 namespace ranges {
+namespace detail {
+
+// template<typename T>
 
 
-} // namespace vccc
+} // namespace detail
+
+/**
+@brief specifies that a range is a view, that is, it has constant time copy/move/assignment
+
+The `%view` concept specifies the requirements of a \ref ranges::range "range" type that has suitable semantic
+properties for use in constructing range adaptor pipelines.
+ */
+template<typename T>
+struct view
+    : conjunction<
+          ranges::range<T>,
+          movable<T>,
+          ranges::enable_view<T>
+      > {};
+
 } // namespace ranges
+} // namespace vccc
 
 #endif // VCCC_RANGES_VIEW_HPP_
