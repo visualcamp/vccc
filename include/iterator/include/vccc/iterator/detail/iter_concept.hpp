4d230544ed6e0c419f183f59723a2850661f83e4
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-01 19:46:01 +0900

    Fix ITER_CONCEPT

--- include/iterator/include/vccc/iterator/detail/iter_concept.hpp
+++ include/iterator/include/vccc/iterator/detail/iter_concept.hpp
@@ -5,30 +5,28 @@
 #ifndef VCCC_ITERATOR_DETAIL_ITER_CONCEPT_HPP_
 #define VCCC_ITERATOR_DETAIL_ITER_CONCEPT_HPP_
 
-#include <iterator>
-
 #include "vccc/iterator/iterator_traits/cxx20_iterator_traits.hpp"
 #include "vccc/iterator/iterator_tag.hpp"
 
 namespace vccc {
 namespace detail {
 
-template<typename I> struct ITER_TRAITS_T { using type = cxx20_iterator_traits<I>; };
-template<typename I> struct ITER_TRAITS_T<std::iterator_traits<I>> { using type = I; };
-template<typename I> struct ITER_TRAITS_T<cxx20_iterator_traits<I>> { using type = I; };
+template<typename I>
+struct ITER_TRAITS_T {
+  using type = std::conditional_t<
+      is_specialized_iterator_traits< cxx20_iterator_traits<I> >::value,
+      cxx20_iterator_traits<I>,
+      I>;
+};
 
 template<typename I>
 using ITER_TRAITS = typename ITER_TRAITS_T<I>::type;
 
 
-template<typename I>
+template<typename I, bool = is_specialized_iterator_traits< cxx20_iterator_traits<I> >::value /* true */>
 struct ITER_CONCEPT_IMPL_3 {};
 template<typename I>
-struct ITER_CONCEPT_IMPL_3<std::iterator_traits<I>> {
-  using type = random_access_iterator_tag;
-};
-template<typename I>
-struct ITER_CONCEPT_IMPL_3<cxx20_iterator_traits<I>> {
+struct ITER_CONCEPT_IMPL_3<I, false> {
   using type = random_access_iterator_tag;
 };
 
