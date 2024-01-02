f015f13aca1968804eb1540a9598571b85551cda
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-02 20:14:02 +0900

    Update views.hpp

--- include/ranges/include/vccc/ranges/views/views.hpp
+++ include/ranges/include/vccc/ranges/views/views.hpp
@@ -5,17 +5,7 @@
 #ifndef VCCC_RANGES_VIEWS_VIEWS_HPP_
 #define VCCC_RANGES_VIEWS_VIEWS_HPP_
 
+#include "vccc/ranges/views/all.hpp"
 #include "vccc/ranges/views/iota.hpp"
-
-namespace vccc {
-namespace ranges {
-namespace views {
-
-} // namespace vccc
-} // namespace ranges
-
-namespace views = ranges::views;
-
-} // namespace views
 
 #endif // VCCC_RANGES_VIEWS_VIEWS_HPP_
