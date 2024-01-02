9739685656c7b216c8fea4a856c1aeac8143796a
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-02 23:03:22 +0900

    Update in_place.hpp

--- include/utility/include/vccc/utility/in_place.hpp
+++ include/utility/include/vccc/utility/in_place.hpp
@@ -6,6 +6,9 @@
 # define VCCC_UTILITY_IN_PLACE_HPP
 #
 # include <cstddef>
+#if __cplusplus >= 201703L
+# include <utility>
+#endif
 #
 # include "vccc/core/inline_or_static.hpp"
 
@@ -14,6 +17,8 @@ namespace vccc {
 //! @addtogroup utility
 //! @{
 
+#if __cplusplus < 201703L
+
 /**
  * @brief in-place construction tag
  */
@@ -21,20 +26,28 @@ struct in_place_t {
   constexpr explicit in_place_t() = default;
 };
 
-VCCC_INLINE_OR_STATIC constexpr in_place_t in_place{};
-
 template<typename T>
 struct in_place_type_t {
   constexpr explicit in_place_type_t() = default;
 };
 
-template<typename T>
-VCCC_INLINE_OR_STATIC constexpr in_place_type_t<T> in_place_type{};
-
 template<std::size_t I>
 struct in_place_index_t {
   constexpr explicit in_place_index_t() = default;
 };
+
+#else
+
+using in_place_t = std::in_place_t;
+template<typename T> using in_place_type_t = std::in_place_type_t<T>;
+template<std::size_t I> using in_place_index_t = std::in_place_index_t<I>;
+
+#endif
+
+VCCC_INLINE_OR_STATIC constexpr in_place_t in_place{};
+
+template<typename T>
+VCCC_INLINE_OR_STATIC constexpr in_place_type_t<T> in_place_type{};
 
 template<std::size_t I>
 VCCC_INLINE_OR_STATIC constexpr in_place_index_t<I> in_place_index{};
