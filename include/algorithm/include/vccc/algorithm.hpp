40361f3eb3b0584a5b028d90fc8f84ba7ca6e8a6
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-10 00:56:59 +0900

    Add ranges::min

--- include/algorithm/include/vccc/algorithm.hpp
+++ include/algorithm/include/vccc/algorithm.hpp
@@ -5,7 +5,10 @@
 #ifndef VCCC_ALGORITHM_HPP
 #define VCCC_ALGORITHM_HPP
 
+#include "vccc/algorithm/min.hpp"
 #include "vccc/algorithm/min_element.hpp"
 #include "vccc/algorithm/swap_ranges.hpp"
+
+/// @defgroup algorithm
 
 #endif // VCCC_ALGORITHM_HPP
