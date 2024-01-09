b6b874b7f53170d380ad1b8eb80b5e92ce815d40
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-10 04:04:55 +0900

    Add ranges::for_each

--- include/algorithm/include/vccc/algorithm.hpp
+++ include/algorithm/include/vccc/algorithm.hpp
@@ -5,6 +5,7 @@
 #ifndef VCCC_ALGORITHM_HPP
 #define VCCC_ALGORITHM_HPP
 
+#include "vccc/__algorithm/for_each.hpp"
 #include "vccc/__algorithm/in_fun_result.hpp"
 #include "vccc/__algorithm/min.hpp"
 #include "vccc/__algorithm/min_element.hpp"
--- test/algorithm_test.cpp
+++ test/algorithm_test.cpp
@@ -4,10 +4,12 @@
 
 #include "test_core.hpp"
 
+#include <string>
 #include <vector>
 
 #include "vccc/algorithm.hpp"
 #include "vccc/string_view.hpp"
+#include "vccc/utility.hpp"
 
 namespace {
 
@@ -16,14 +18,51 @@ int Test() {
 
   namespace ranges = vccc::ranges;
   using namespace vccc::string_view_literals;
+  {
+    TEST_ENSURES((ranges::min(1, 9999) == 1));
+    TEST_ENSURES((ranges::min('a', 'b') == 'a'));
+    TEST_ENSURES(ranges::min({"foo"_sv, "bar"_sv, "hello"_sv}, {}, &vccc::string_view::size) == "foo"_sv);
 
-  TEST_ENSURES((ranges::min(1, 9999) == 1));
-  TEST_ENSURES((ranges::min('a', 'b') == 'a'));
-  TEST_ENSURES(ranges::min({"foo"_sv, "bar"_sv, "hello"_sv}, {}, &vccc::string_view::size) == "foo"_sv);
+    std::vector<int> v = {1, 2, 3, 4};
+    TEST_ENSURES(ranges::min_element(v) == v.begin());
+    TEST_ENSURES(ranges::min(v) == 1);
+  }
 
-  std::vector<int> v = {1, 2, 3, 4};
-  TEST_ENSURES(ranges::min_element(v) == v.begin());
-  TEST_ENSURES(ranges::min(v) == 1);
+  { // ranges::for_each
+    std::vector<int> nums = {1, 2, 3, 4, 5};
+    auto print = [](const auto& x) { std::cout << ' ' << x; };
+
+    ranges::for_each(vccc::as_const(nums), print);
+    print('\n');
+    struct Sum {
+      void operator()(int n) { sum += n; }
+      int sum {0};
+    };
+
+    ranges::for_each(nums, [](auto& x) { ++x; });
+    TEST_ENSURES(nums.front() == 2);
+    TEST_ENSURES(nums.back() == 6);
+
+    auto result = ranges::for_each(nums.begin(), nums.end(), Sum{});
+    TEST_ENSURES(result.in == nums.end());
+    TEST_ENSURES(result.fun.sum == 20);
+
+    ranges::for_each(vccc::as_const(nums), print);
+    print('\n');
+
+    using pair = vccc::compressed_pair<int, std::string>;
+    std::vector<pair> pairs = {{1, "one"}, {2, "two"}, {3, "three"}};
+
+    std::cout << "project the pair::first";
+    ranges::for_each(pairs, print, [](const pair& p) { return p.first(); });
+    print('\n');
+
+    using std_pair = std::pair<int, std::string>;
+    std::vector<std_pair> pairs2 = {{1, "one"}, {2, "two"}, {3, "three"}};
+    std::cout << "project the pair::second";
+    ranges::for_each(pairs2, print, &std_pair::second);
+    print('\n');
+  }
 
   return TEST_RETURN_RESULT;
 }
