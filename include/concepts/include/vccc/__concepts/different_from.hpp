bf83f61a44f8d29d89f04f8cc810cfc86c605be0
lackhole <cosgenio@gmail.com>
2024-01-18 20:29:44 +0900

    Create different_from.hpp

---
+++ include/concepts/include/vccc/__concepts/different_from.hpp
@@ -0,0 +1,24 @@
+//
+// Created by YongGyu Lee on 1/18/24.
+//
+
+#ifndef VCCC_CONCEPTS_DIFFERENT_FROM_HPP
+#define VCCC_CONCEPTS_DIFFERENT_FROM_HPP
+
+#include "vccc/__concepts/same_as.hpp"
+#include "vccc/__type_traits/negation.hpp"
+#include "vccc/__type_traits/remove_cvref.hpp"
+
+namespace vccc {
+
+/// @addtogroup concepts
+/// @{
+
+template<typename Test, typename From>
+struct different_from : negation< same_as<remove_cvref_t<Test>, From> > {};
+
+/// @}
+
+} // namespace vccc
+
+#endif // VCCC_CONCEPTS_DIFFERENT_FROM_HPP
