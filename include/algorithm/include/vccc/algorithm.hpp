fe4d24517c7f0d6c06c0b4f1306deeffb634f014
lackhole <cosgenio@gmail.com>
2024-01-18 21:01:53 +0900

    Add ranges::find

--- include/algorithm/include/vccc/algorithm.hpp
+++ include/algorithm/include/vccc/algorithm.hpp
@@ -7,6 +7,7 @@
 
 #include "vccc/__algorithm/copy.hpp"
 #include "vccc/__algorithm/equal.hpp"
+#include "vccc/__algorithm/find.hpp"
 #include "vccc/__algorithm/for_each.hpp"
 #include "vccc/__algorithm/in_fun_result.hpp"
 #include "vccc/__algorithm/in_out_result.hpp"
