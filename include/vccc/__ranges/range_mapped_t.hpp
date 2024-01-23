a887e36dc2abff07b0715b658355320e6dbf2a1c
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-24 01:19:39 +0900

    Update range_mapped_t.hpp

--- include/vccc/__ranges/range_mapped_t.hpp
+++ include/vccc/__ranges/range_mapped_t.hpp
@@ -5,6 +5,9 @@
 #ifndef VCCC_RANGES_RANGE_MAPPED_T_HPP
 #define VCCC_RANGES_RANGE_MAPPED_T_HPP
 
+#include <tuple>
+#include <type_traits>
+
 #include "vccc/__ranges/input_range.hpp"
 #include "vccc/__ranges/range_value_t.hpp"
 #include "vccc/__type_traits/void_t.hpp"
@@ -13,12 +16,31 @@ namespace vccc {
 namespace ranges {
 namespace detail {
 
-template<typename R, bool = input_range<R>::value /* false */, typename = void>
+template<typename T, typename = void>
+struct has_second_type : std::false_type {};
+template<typename T>
+struct has_second_type<T, void_t<typename T::second_type>> : std::true_type {};
+
+template<typename T, typename = void>
+struct has_second_tuple_element : std::false_type {};
+template<typename T>
+struct has_second_tuple_element<T, void_t<std::tuple_element_t<1, T>>> : std::true_type {};
+
+template<typename RV, bool = has_second_type<RV>::value, bool = has_second_tuple_element<RV>::value>
+struct guide_map {};
+template<typename RV, bool v>
+struct guide_map<RV, true, v> {
+  using type = typename RV::second_type;
+};
+template<typename RV>
+struct guide_map<RV, false, true> {
+  using type = std::tuple_element_t<1, RV>;
+};
+
+template<typename R, bool = input_range<R>::value /* false */>
 struct range_mapped_impl {};
 template<typename R>
-struct range_mapped_impl<R, true, void_t<typename range_value_t<R>::second_type>> {
- using type = typename range_value_t<R>::second_type;
-};
+struct range_mapped_impl<R, true> : guide_map<range_value_t<R>> {};
 
 } // namespace ranges
 
