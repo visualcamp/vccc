078ad58e690c1083f86b3d9bf44360d58cf88400
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-26 23:32:52 +0900

    Add mergeable

--- include/vccc/iterator.hpp
+++ include/vccc/iterator.hpp
@@ -55,6 +55,7 @@
 #include "vccc/__iterator/iter_val_t.hpp"
 #include "vccc/__iterator/iter_value_t.hpp"
 #include "vccc/__iterator/iterator_tag.hpp"
+#include "vccc/__iterator/mergeable.hpp"
 #include "vccc/__iterator/next.hpp"
 #include "vccc/__iterator/output_iterator.hpp"
 #include "vccc/__iterator/permutable.hpp"
