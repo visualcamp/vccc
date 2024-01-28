3a2a4a3e7cd3ede9f93d2c8df7d26e97acc02d05
Yong Gyu Lee <cosgenio@gmail.com>
2024-01-28 19:11:53 +0900

    Add projectable

--- include/vccc/__iterator/projected.hpp
+++ include/vccc/__iterator/projected.hpp
@@ -8,9 +8,10 @@
 #include "vccc/__iterator/indirect_result_t.hpp"
 #include "vccc/__iterator/indirectly_readable.hpp"
 #include "vccc/__iterator/indirectly_regular_unary_invocable.hpp"
+#include "vccc/__iterator/iter_difference_t.hpp"
 #include "vccc/__iterator/weakly_incrementable.hpp"
 #include "vccc/__type_traits/conjunction.hpp"
-#include "vccc/__type_traits/is_complete.hpp"
+#include "vccc/__type_traits/has_typename_type.hpp"
 #include "vccc/__type_traits/remove_cvref.hpp"
 
 namespace vccc {
@@ -29,28 +30,15 @@ template<
     bool = conjunction<
                indirectly_readable<I>,
                indirectly_regular_unary_invocable<Proj, I>
-           >::value
+           >::value /* true */
 >
-struct projected_impl_2 {
+struct projected_impl {
   struct type : projected_impl_difference_type<I> {
     using value_type = remove_cvref_t<indirect_result_t<Proj, I>>;
 
     indirect_result_t<Proj&, I> operator*() const;
   };
 };
-
-template<typename I, typename Proj>
-struct projected_impl_2<I, Proj, false> {};
-
-template<
-    typename I,
-    typename Proj,
-    bool = conjunction<
-               is_complete<I>,
-               is_complete<Proj>
-           >::value /* true */
->
-struct projected_impl : projected_impl_2<I, Proj> {};
 
 template<typename I, typename Proj>
 struct projected_impl<I, Proj, false> {};
@@ -62,19 +50,25 @@ struct projected_impl<I, Proj, false> {};
 @{
 @addtogroup iterator_projected__class__projected projected
 @brief helper template for specifying the constraints on algorithms that accept projections
+
 `%projected` is used only to constrain algorithms that accept callable objects and projections,
 and hence its `operator*()` is not defined.
 
-<H1>Notes</H1>
-`vccc::projected` meets the requirements of C++ 26
+@warning `%vccc::projected` meets the requirements of C++ 26, but without using
+[constraints](https://en.cppreference.com/w/cpp/language/constraints) user must check its validity with
+`vccc::projectable<I, Proj>`before using `%vccc::projected`
 
 @sa [std::projected](https://en.cppreference.com/w/cpp/iterator/projected)
 
 @{
- */
+*/
 
 template<typename I, typename Proj>
-using projected = typename detail::projected_impl_2<I, Proj>::type;
+using projected = typename detail::projected_impl<I, Proj>::type;
+
+/// @brief Test if expression `vccc::projected<I, Proj>` is valid in an unqualified context
+template<typename I, typename Proj>
+using projectable = has_typename_type<detail::projected_impl<I, Proj>>;
 
 /// @}
 /// @}
