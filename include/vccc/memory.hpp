2364a9f5f292445c919793396b949214e2fc387c
Yong Gyu Lee <cosgenio@gmail.com>
2024-02-07 00:47:43 +0900

    Create memory.hpp

---
+++ include/vccc/memory.hpp
@@ -0,0 +1,11 @@
+//
+// Created by yonggyulee on 2/7/24.
+//
+
+#ifndef VCCC_MEMORY_HPP_
+#define VCCC_MEMORY_HPP_
+
+#include "vccc/__memory/addressof.hpp"
+#include "vccc/__memory/to_address.hpp"
+
+#endif // VCCC_MEMORY_HPP_
