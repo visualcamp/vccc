2a52902c16bd8d8c144215ff083a1501884889b1
lackhole <cosgenio@gmail.com>
2024-02-08 19:34:13 +0900

    Update ranges_test.cpp

--- test/ranges_test.cpp
+++ test/ranges_test.cpp
@@ -970,9 +970,9 @@ int main() {
       constexpr auto common1 { v1 | vccc::views::common };
       TEST_ENSURES(common1.size() == 5);
 
-      // constexpr auto take3 { v1 | vccc::views::reverse | vccc::views::take(3) };
-      // constexpr auto common2 { take3 | vccc::views::common };
-      // static_assert(common2.size() == 3);
+      constexpr auto take3 { v1 | vccc::views::reverse | vccc::views::take(3) };
+      constexpr auto common2 { take3 | vccc::views::common };
+      static_assert(common2.size() == 3);
 
       constexpr static auto v2 = { "∧"_sv, "∨"_sv, "∃"_sv, "∀"_sv };
       TEST_ENSURES(vccc::ranges::views::common(v2).size() == 4);
