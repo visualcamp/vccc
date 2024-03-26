9f80590bf274b776a45a29375903106beffb2b07
lackhole <cosgenio@gmail.com>
2024-03-26 14:06:58 +0900

    Fix join_with

--- include/vccc/__ranges/views/join_with.hpp
+++ include/vccc/__ranges/views/join_with.hpp
@@ -8,46 +8,90 @@
 #include <type_traits>
 #include <utility>
 
+#include "vccc/__concepts/derived_from_single_crtp.hpp"
 #include "vccc/__concepts/different_from.hpp"
 #include "vccc/__ranges/range.hpp"
 #include "vccc/__ranges/range_adaptor_closure.hpp"
 #include "vccc/__ranges/range_reference_t.hpp"
 #include "vccc/__ranges/range_value_t.hpp"
+#include "vccc/__ranges/view.hpp"
+#include "vccc/__ranges/view_interface.hpp"
 #include "vccc/__ranges/viewable_range.hpp"
 #include "vccc/__ranges/views/all.hpp"
 #include "vccc/__ranges/views/join_with_view.hpp"
 #include "vccc/__ranges/views/single.hpp"
+#include "vccc/__type_traits/is_implicitly_constructible.hpp"
 
 namespace vccc {
 namespace ranges {
 namespace views {
 namespace detail {
 
+template<typename T>
+struct first_template_arg;
+template<template<typename T, typename...> class Class, typename T1, typename... Ts>
+struct first_template_arg<Class<T1, Ts...>> {
+  static_assert(derived_from_single_crtp<Class<T1, Ts...>, view_interface>::value, "Class must be view");
+  using type = T1;
+};
+
+template<typename T>
+using first_template_arg_t = typename first_template_arg<T>::type;
+
 template<typename Pattern>
-class join_with_adaptor_object : public range_adaptor_closure<join_with_adaptor_object<Pattern>> {
+class join_with_closure : public range_adaptor_closure<join_with_closure<Pattern>> {
+  using prv = first_template_arg_t<Pattern>;
+
  public:
-  template<typename U, std::enable_if_t<different_from<join_with_adaptor_object, U>::value, int> = 0>
-  constexpr explicit join_with_adaptor_object(U&& pattern)
+  template<typename U, std::enable_if_t<different_from<join_with_closure, U>::value, int> = 0>
+  constexpr explicit join_with_closure(U&& pattern)
       : pattern_(std::forward<U>(pattern)) {}
 
-  template<typename R, std::enable_if_t<range<R>::value, int> = 0>
-  constexpr auto operator()(R&& r) const& {
-    return join_with_view<all_t<R>, all_t<const Pattern&>>{std::forward<R>(r), pattern_};
+  template<typename R, std::enable_if_t<viewable_range<R>::value, int> = 0>
+  constexpr auto operator()(R&& r) & {
+    return call(*this, std::forward<R>(r), is_implicitly_constructible<range_value_t<range_reference_t<R>>, prv&>{});
   }
 
-  template<typename R, std::enable_if_t<range<R>::value, int> = 0>
+  template<typename R, std::enable_if_t<viewable_range<R>::value, int> = 0>
+  constexpr auto operator()(R&& r) const& {
+    return call(*this, std::forward<R>(r), is_implicitly_constructible<range_value_t<range_reference_t<R>>, const prv&>{});
+  }
+
+  template<typename R, std::enable_if_t<viewable_range<R>::value, int> = 0>
   constexpr auto operator()(R&& r) && {
-    return join_with_view<all_t<R>, all_t<Pattern>>{std::forward<R>(r), std::move(pattern_)};
+    return call(std::move(*this), std::forward<R>(r), is_implicitly_constructible<range_value_t<range_reference_t<R>>, prv&&>{});
+  }
+
+  template<typename R, std::enable_if_t<viewable_range<R>::value, int> = 0>
+  constexpr auto operator()(R&& r) const&& {
+    return call(std::move(*this), std::forward<R>(r), is_implicitly_constructible<range_value_t<range_reference_t<R>>, const prv&&>{});
   }
 
  private:
+  constexpr decltype(auto) value_ref() & { return *pattern_.begin(); }
+  constexpr decltype(auto) value_ref() const& { return *pattern_.begin(); }
+  constexpr decltype(auto) value_ref() && { return std::move(*pattern_.begin()); }
+  constexpr decltype(auto) value_ref() const&& { return std::move(*pattern_.begin()); }
+
+  template<typename This, typename R>
+  constexpr static auto call(This&& thiz, R&& r, std::true_type) {
+    return join_with_view<all_t<R>, single_view<range_value_t<range_reference_t<R>>>>(
+        std::forward<R>(r), std::forward<This>(thiz).value_ref());
+  }
+
+  template<typename This, typename R>
+  constexpr static auto call(This&& thiz, R&& r, std::false_type) {
+    return join_with_view<all_t<R>, Pattern>{std::forward<R>(r), std::forward<This>(thiz).pattern_};
+  }
+
   Pattern pattern_;
 };
 
 struct join_with_niebloid {
   template<typename R, typename Pattern, std::enable_if_t<conjunction<
       viewable_range<R>,
-      different_from<Pattern, range_value_t<range_reference_t<R>>>
+      has_typename_type<all_t_impl<Pattern>>,
+      negation<is_implicitly_constructible<range_value_t<range_reference_t<R>>, Pattern>>
   >::value, int> = 0>
   constexpr auto operator()(R&& r, Pattern&& pattern) const {
     return join_with_view<all_t<R>, all_t<Pattern>>{std::forward<R>(r), std::forward<Pattern>(pattern)};
@@ -63,7 +107,18 @@ struct join_with_niebloid {
 
   template<typename Pattern>
   constexpr auto operator()(Pattern&& pattern) const {
-    return join_with_adaptor_object<std::remove_reference_t<Pattern>>{std::forward<Pattern>(pattern)};
+    return create_closure(std::forward<Pattern>(pattern), has_typename_type<all_t_impl<Pattern>>{});
+  }
+
+ private:
+  template<typename Pattern>
+  constexpr auto create_closure(Pattern&& pattern, std::true_type /* all */) const {
+    return join_with_closure<views::all_t<Pattern>>(std::forward<Pattern>(pattern));
+  }
+
+  template<typename Pattern>
+  constexpr auto create_closure(Pattern&& pattern, std::false_type /* all */) const {
+    return join_with_closure<decltype(views::single(std::forward<Pattern>(pattern)))>(std::forward<Pattern>(pattern));
   }
 };
 
