ebf04e2521b9e1c8934fb80ae79a9009635e0ec5
lackhole <cosgenio@gmail.com>
2024-03-27 18:32:20 +0900

    Create crbegin.hpp

---
+++ include/vccc/__ranges/crbegin.hpp
@@ -0,0 +1,47 @@
+//
+// Created by YongGyu Lee on 3/27/24.
+//
+
+#ifndef VCCC_RANGES_CRBEGIN_HPP_
+#define VCCC_RANGES_CRBEGIN_HPP_
+
+#include <type_traits>
+
+#include "vccc/__core/inline_or_static.hpp"
+#include "vccc/__iterator/basic_const_iterator.hpp"
+#include "vccc/__ranges/enable_borrowed_range.hpp"
+#include "vccc/__ranges/possibly_const_range.hpp"
+#include "vccc/__ranges/rbegin.hpp"
+#include "vccc/__type_traits/disjunction.hpp"
+
+namespace vccc {
+namespace ranges {
+namespace detail {
+
+struct crbegin_niebloid {
+  template<typename T, std::enable_if_t<disjunction<
+      std::is_lvalue_reference<T>,
+      enable_borrowed_range<std::remove_cv_t<T>>
+  >::value, int> = 0>
+  constexpr auto operator()(T&& t) const {
+    return vccc::const_iterator<decltype(ranges::rbegin(possibly_const_range(t)))>(ranges::rbegin(possibly_const_range(t)));
+  }
+};
+
+} // namespace detail
+
+inline namespace niebloid {
+
+/// @addtogroup ranges
+/// @{
+
+VCCC_INLINE_OR_STATIC constexpr detail::crbegin_niebloid crbegin{};
+
+/// @}
+
+} // inline namespace niebloid
+
+} // namespace ranges
+} // namespace vccc
+
+#endif // VCCC_RANGES_CRBEGIN_HPP_
