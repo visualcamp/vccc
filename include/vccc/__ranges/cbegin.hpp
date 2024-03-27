9f18cd7f59ed10a2c5c0be918495afe5805f4370
lackhole <cosgenio@gmail.com>
2024-03-27 18:45:36 +0900

    Fix cbegin

--- include/vccc/__ranges/cbegin.hpp
+++ include/vccc/__ranges/cbegin.hpp
@@ -10,11 +10,9 @@
 #include "vccc/__core/inline_or_static.hpp"
 #include "vccc/__iterator/basic_const_iterator.hpp"
 #include "vccc/__ranges/begin.hpp"
-#include "vccc/__ranges/constant_range.hpp"
 #include "vccc/__ranges/enable_borrowed_range.hpp"
-#include "vccc/__type_traits/detail/tag.hpp"
+#include "vccc/__ranges/possibly_const_range.hpp"
 #include "vccc/__type_traits/disjunction.hpp"
-#include "vccc/__utility/as_const.hpp"
 
 namespace vccc {
 namespace ranges {
@@ -26,22 +24,7 @@ struct cbegin_niebloid {
       enable_borrowed_range<std::remove_cv_t<T>>
   >::value, int> = 0>
   constexpr auto operator()(T&& t) const {
-    using tag = conditional_tag<constant_range<T>, constant_range<const T>>;
-    return this->call(std::forward<T>(t), tag{});
-  }
-
- private:
-  template<typename T>
-  constexpr auto call(T&& t, vccc::detail::tag_1) const {
-    return ranges::begin(t);
-  }
-  template<typename T>
-  constexpr auto call(T&& t, vccc::detail::tag_2) const {
-    return ranges::begin(vccc::as_const(t));
-  }
-  template<typename T>
-  constexpr auto call(T&& t, vccc::detail::tag_else) const {
-    return make_const_iterator(ranges::begin(t));
+    return vccc::const_iterator<decltype(ranges::begin(vccc::ranges::possibly_const_range(t)))>(ranges::begin(vccc::ranges::possibly_const_range(t)));
   }
 };
 
