3da331ab77d3bd42b4da455f2bfe2b92a1bd3a82
lackhole <cosgenio@gmail.com>
2024-03-27 18:30:10 +0900

    Update possibly_const_range.hpp

--- include/vccc/__ranges/possibly_const_range.hpp
+++ include/vccc/__ranges/possibly_const_range.hpp
@@ -17,19 +17,19 @@ namespace ranges {
 namespace detail {
 
 template<typename R>
-constexpr auto possibly_const_range_impl(R& r, std::true_type) noexcept {
+constexpr auto& possibly_const_range_impl(R& r, std::true_type) noexcept {
   return const_cast<const R&>(r);
 }
 
 template<typename R>
-constexpr auto possibly_const_range_impl(R& r, std::false_type) noexcept {
+constexpr auto& possibly_const_range_impl(R& r, std::false_type) noexcept {
   return r;
 }
 
 } // namespace detail
 
 template<typename R, std::enable_if_t<input_range<R>::value, int> = 0>
-constexpr auto possibly_const_range(R& r) noexcept {
+constexpr auto& possibly_const_range(R& r) noexcept {
   return detail::possibly_const_range_impl(r, conjunction<constant_range<const R>, negation<constant_range<R>>>{});
 }
 
