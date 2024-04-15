e7edfe68ea08cee91016fc8b173d2fa0c6b92d54
lackhole <cosgenio@gmail.com>
2024-04-15 16:33:54 +0900

    Create ignore_dumb_diagnostics_pop.hpp

---
+++ include/vccc/__core/ignore_dumb_diagnostics_pop.hpp
@@ -0,0 +1,12 @@
+//
+// Created by YongGyu Lee on 4/12/24.
+//
+
+#ifndef VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
+#define VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#endif // VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
