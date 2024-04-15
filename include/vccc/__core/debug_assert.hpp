6bdc916c6fc93996bafbea53fab9f0e270b84935
lackhole <cosgenio@gmail.com>
2024-04-15 16:32:29 +0900

    Create debug_assert.hpp

---
+++ include/vccc/__core/debug_assert.hpp
@@ -0,0 +1,22 @@
+//
+// Created by YongGyu Lee on 4/15/24.
+//
+
+#ifndef VCCC_CORE_DEBUG_ASSERT_HPP_
+#define VCCC_CORE_DEBUG_ASSERT_HPP_
+
+#include <cassert>
+
+#define VCCC_LINE_LOCATION_IMPL2(file, line) \
+    "file: ", file ", line " # line
+
+#define VCCC_LINE_LOCATION_IMPL(file, line) \
+    VCCC_LINE_LOCATION_IMPL2(file, line)
+
+#define VCCC_LINE_LOCATION \
+    VCCC_LINE_LOCATION_IMPL(__FILE__, __LINE__)
+
+#define VCCC_DEBUG_ASSERT(expr) \
+    cassert(((void) "Assertion on " VCCC_LINE_LOCATION, expr))
+
+#endif // VCCC_CORE_DEBUG_ASSERT_HPP_
