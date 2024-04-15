0b5551c2196d51e0e2161d1e357d2c3fd3779fdc
lackhole <cosgenio@gmail.com>
2024-04-15 16:33:55 +0900

    Create ignore_dumb_diagnostics_push.hpp

---
+++ include/vccc/__core/ignore_dumb_diagnostics_push.hpp
@@ -0,0 +1,13 @@
+//
+// Created by YongGyu Lee on 4/12/24.
+//
+
+#ifndef VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
+#define VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundefined-internal"
+#endif
+
+#endif // VCCC_CORE_IGNORE_DUMB_DIAGNOSTICS_PUSH_HPP_
