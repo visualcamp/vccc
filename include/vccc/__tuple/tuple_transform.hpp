1f21ba2ec1f9637e36e1f596a53641b4287531ec
lackhole <cosgenio@gmail.com>
2024-04-15 16:34:12 +0900

    Update tuple_transform.hpp

--- include/vccc/__tuple/tuple_transform.hpp
+++ include/vccc/__tuple/tuple_transform.hpp
@@ -9,13 +9,19 @@
 #include <utility>
 
 #include "vccc/__functional/invoke.hpp"
+#include "vccc/__type_traits/bool_constant.hpp"
+#include "vccc/__type_traits/conjunction.hpp"
 #include "vccc/__type_traits/remove_cvref.hpp"
 
 namespace vccc {
 namespace detail {
 
 template<typename Tuple, typename F, std::size_t... I>
-constexpr auto tuple_transform_impl(Tuple&& t, F&& f, std::index_sequence<I...>) {
+constexpr auto tuple_transform_impl(Tuple&& t, F&& f, std::index_sequence<I...>)
+    noexcept(conjunction<
+        bool_constant<noexcept(vccc::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t))))>...
+    >::value)
+{
   return std::tuple<decltype(vccc::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t))))...>(
       vccc::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t)))...);
 }
@@ -27,7 +33,15 @@ constexpr auto tuple_transform_impl(Tuple&& t, F&& f, std::index_sequence<I...>)
 
 /// @brief Constructs a new tuple with each elements transformed
 template<typename Tuple, typename F>
-constexpr auto tuple_transform(Tuple&& t, F&& f) {
+constexpr auto tuple_transform(Tuple&& t, F&& f)
+    noexcept(noexcept(
+        detail::tuple_transform_impl(
+            std::forward<Tuple>(t),
+            std::forward<F>(f),
+            std::make_index_sequence<std::tuple_size<remove_cvref_t<Tuple>>::value>{}
+        )
+    ))
+{
   return detail::tuple_transform_impl(
       std::forward<Tuple>(t), std::forward<F>(f),
       std::make_index_sequence<std::tuple_size<remove_cvref_t<Tuple>>::value>{});
