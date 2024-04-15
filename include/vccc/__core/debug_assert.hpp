b5823e1e75db2436d57f414f609fccaa0ba70f6b
lackhole <cosgenio@gmail.com>
2024-04-15 18:49:28 +0900

    Update debug_assert.hpp

--- include/vccc/__core/debug_assert.hpp
+++ include/vccc/__core/debug_assert.hpp
@@ -8,7 +8,7 @@
 #include <cassert>
 
 #define VCCC_LINE_LOCATION_IMPL2(file, line) \
-    "file: ", file ", line " # line
+    "file: " file ", line " # line
 
 #define VCCC_LINE_LOCATION_IMPL(file, line) \
     VCCC_LINE_LOCATION_IMPL2(file, line)
@@ -17,6 +17,6 @@
     VCCC_LINE_LOCATION_IMPL(__FILE__, __LINE__)
 
 #define VCCC_DEBUG_ASSERT(expr) \
-    cassert(((void) "Assertion on " VCCC_LINE_LOCATION, expr))
+    assert(((void) "Assertion on " VCCC_LINE_LOCATION, expr))
 
 #endif // VCCC_CORE_DEBUG_ASSERT_HPP_
