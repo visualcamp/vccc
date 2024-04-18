06602b1f275baacf0efce89eb3bf82c5dac4a49e
Yong Gyu Lee <cosgenio@gmail.com>
2024-04-18 12:28:00 +0900

    Update saturate_cast.hpp

--- include/vccc/__numeric/saturate_cast.hpp
+++ include/vccc/__numeric/saturate_cast.hpp
@@ -17,9 +17,9 @@ namespace detail {
 // high bit -> low bit
 template<typename To, typename From, typename Any>
 inline constexpr To saturate_cast_impl(From x, Any, Any, std::true_type /* digit<To>  <  digit<From> */) noexcept {
-  if (x < static_cast<From>(std::numeric_limits<To>::min()))
+  if (x < static_cast<From>((std::numeric_limits<To>::min)()))
     return std::numeric_limits<To>::min();
-  if (x > static_cast<From>(std::numeric_limits<To>::max()))
+  if (x > static_cast<From>((std::numeric_limits<To>::max)()))
     return std::numeric_limits<To>::max();
   return static_cast<To>(x);
 }
@@ -33,8 +33,8 @@ inline constexpr To saturate_cast_impl(From x, Any, Any, std::false_type /* digi
 // unsigned -> signed
 template<typename To, typename From, typename Any>
 inline constexpr To saturate_cast_impl(From x, std::false_type /* signed */, std::true_type /* signed */, Any) noexcept {
-  if (x > std::make_unsigned_t<To>(std::numeric_limits<To>::max()))
-    return std::numeric_limits<To>::max();
+  if (x > std::make_unsigned_t<To>((std::numeric_limits<To>::max)()))
+    return (std::numeric_limits<To>::max)();
   return static_cast<To>(x);
 }
 
@@ -43,8 +43,8 @@ template<typename To, typename From, typename Any>
 inline constexpr To saturate_cast_impl(From x, std::true_type /* signed */, std::false_type /* signed */, Any) noexcept {
   if (x < 0)
     return 0;
-  if (std::make_unsigned_t<From>(x) > std::numeric_limits<To>::max())
-    return std::numeric_limits<To>::max();
+  if (std::make_unsigned_t<From>(x) > (std::numeric_limits<To>::max)())
+    return (std::numeric_limits<To>::max)();
   return static_cast<To>(x);
 }
 
