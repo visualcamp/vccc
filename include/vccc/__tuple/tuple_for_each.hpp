8d339dd302c06ddcebe169f38680d38fff5b6f3e
lackhole <cosgenio@gmail.com>
2024-05-13 20:35:48 +0900

    Add tuple_for_each_index

--- include/vccc/__tuple/tuple_for_each.hpp
+++ include/vccc/__tuple/tuple_for_each.hpp
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <tuple>
 #include <utility>
+#include <type_traits>
 
 #include "vccc/__functional/invoke.hpp"
 #include "vccc/__utility/in_place.hpp"
@@ -19,21 +20,6 @@
 namespace vccc {
 namespace detail {
 
-template<typename Tuple, typename F, std::size_t... I>
-constexpr void tuple_for_each_impl(Tuple&& t, F&& f, std::index_sequence<I...>, std::true_type) {
-  int dummy[] = {
-      ((void)vccc::invoke(f, std::get<I>(std::forward<Tuple>(t)), in_place_index<I>), 0)...
-  };
-}
-
-template<typename Tuple, typename F, std::size_t... I>
-constexpr void tuple_for_each_impl(Tuple&& t, F&& f, std::index_sequence<I...>, std::false_type) {
-  int dummy[] = {
-      (vccc::invoke(f, std::get<I>(std::forward<Tuple>(t))), 0)...
-  };
-  (void)dummy;
-}
-
 template<typename Tuple>
 using tuple_index_sequence = std::make_index_sequence<std::tuple_size<remove_cvref_t<Tuple>>::value>;
 
@@ -44,11 +30,19 @@ template<typename Tuple, typename F, std::size_t... I>
 struct tuple_for_each_invocable_impl<Tuple, F, std::index_sequence<I...>>
     : conjunction<
         is_invocable<F, decltype(std::get<I>(std::declval<Tuple>()))>...
-    > {};
+      > {};
 
 template<typename Tuple, typename F>
 struct tuple_for_each_invocable
     : tuple_for_each_invocable_impl<Tuple, F, tuple_index_sequence<Tuple>> {};
+
+template<typename Tuple, typename F, std::size_t... I>
+constexpr void tuple_for_each_impl(std::index_sequence<I...>, Tuple&& t, F&& f) {
+  int dummy[] = {
+      (vccc::invoke(f, std::get<I>(std::forward<Tuple>(t))), 0)...
+  };
+  (void)dummy;
+}
 
 template<typename Tuple, typename F, typename Seq>
 struct tuple_for_each_in_place_index_invocable_impl;
@@ -56,25 +50,46 @@ struct tuple_for_each_in_place_index_invocable_impl;
 template<typename Tuple, typename F, std::size_t... I>
 struct tuple_for_each_in_place_index_invocable_impl<Tuple, F, std::index_sequence<I...>>
     : conjunction<
-        is_invocable<F, decltype(std::get<I>(std::declval<Tuple>())), in_place_index_t<I>>...
-> {};
+        is_invocable<F, decltype(std::get<I>(std::declval<Tuple>())), std::integral_constant<std::size_t, I>>...
+      > {};
 
 template<typename Tuple, typename F>
 struct tuple_for_each_in_place_index_invocable
     : tuple_for_each_in_place_index_invocable_impl<Tuple, F, tuple_index_sequence<Tuple>> {};
+
+template<typename Tuple, typename F, std::size_t... I>
+constexpr void tuple_for_each_index_impl(std::index_sequence<I...>, Tuple&& t, F&& f) {
+  int dummy[] = {
+      (vccc::invoke(f, std::get<I>(std::forward<Tuple>(t)), std::integral_constant<std::size_t, I>{}), 0)...
+  };
+  (void)dummy;
+}
 
 } // namespace detail
 
 /// @addtogroup tuple
 /// @{
 
+// Invokes f(std::get<i>(t)) for each i in [0, std::tuple_size<Tuple>)
 template<typename Tuple, typename F>
-constexpr std::enable_if_t<detail::tuple_for_each_invocable<Tuple, F>::value> tuple_for_each(Tuple&& t, F&& f) {
+constexpr std::enable_if_t<detail::tuple_for_each_invocable<Tuple, F>::value>
+tuple_for_each(Tuple&& t, F&& f) {
   return vccc::detail::tuple_for_each_impl(
-      std::forward<Tuple>(t),
-      std::forward<F>(f),
       detail::tuple_index_sequence<Tuple>{},
-      detail::tuple_for_each_in_place_index_invocable<Tuple, F>{});
+      std::forward<Tuple>(t),
+      std::forward<F>(f)
+  );
+}
+
+// Invokes f(std::get<i>(t), std::integral_constant<std::size_t, i>{}) for each i in [0, std::tuple_size<Tuple>)
+template<typename Tuple, typename F>
+constexpr std::enable_if_t<detail::tuple_for_each_in_place_index_invocable<Tuple, F>::value>
+tuple_for_each_index(Tuple&& t, F&& f) {
+  return vccc::detail::tuple_for_each_index_impl(
+      detail::tuple_index_sequence<Tuple>{},
+      std::forward<Tuple>(t),
+      std::forward<F>(f)
+  );
 }
 
 /// @}
